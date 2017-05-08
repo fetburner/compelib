64d3479e21c5fc6f1e3f92aa3ead6c5f03faae3d
fetburner <masayuki2718@yahoo.co.jp>
2017-05-09 08:02:43 +0900

    Refactor binary search

--- bsearch.v
+++ bsearch.v
@@ -1,111 +1,103 @@
-Require Import Arith Div2 Omega Recdef.
+Require Import Arith Omega Recdef.
 
-Function bsearch_aux (p : nat -> bool) offset n { wf lt n } :=
-  match n with
-  | O => offset
-  | _ =>
-      let m := div2 n in
-      if p (offset + m) then bsearch_aux p offset m
-      else bsearch_aux p (S m + offset) (n - S m)
-  end.
-Proof.
-  + intros. apply lt_div2. omega.
-  + intros. omega.
-  + apply lt_wf.
-Defined.
+Section LowerBound.
+  Variable P : nat -> Prop.
+  Variable threshold : nat.
+  Hypothesis P_dec : forall n, { P n } + { ~P n }.
+  Hypothesis P_monotone1 : forall n, threshold <= n -> P n.
+  Hypothesis P_monotone2 : forall n, P n -> threshold <= n.
 
-Lemma bsearch_aux_spec : forall p n offset error,
-  (forall n, offset + error <= n -> p n = true) ->
-  (forall n, p n = true -> offset + error <= n) ->
-  error <= n ->
-  bsearch_aux p offset n = offset + error.
-Proof.
-  intros p n.
-  induction n as [[| n'] IHn] using lt_wf_ind;
-    intros offset error H H' ?;
-    rewrite bsearch_aux_equation.
-  - omega.
-  - remember (p (offset + div2 (S n'))) as b.
-    symmetry in Heqb.
-    destruct b.
-    + apply IHn; eauto.
-      * apply lt_div2.
+  Function lower_bound_aux offset n { wf lt n } :=
+    match n with
+    | O => offset
+    | _ =>
+      let m := Nat.div2 n in
+      if P_dec (offset + m) then lower_bound_aux offset m
+      else lower_bound_aux (S m + offset) (n - S m)
+    end.
+  Proof.
+    - intros. apply Nat.lt_div2. omega.
+    - intros. omega.
+    - apply lt_wf.
+  Defined.
+
+  Lemma lower_bound_aux_spec n : forall offset error,
+    threshold = offset + error ->
+    error <= n ->
+    lower_bound_aux offset n = offset + error.
+  Proof.
+    induction n as [[| n'] IHn] using lt_wf_ind;
+      intros offset error ? ?;
+      subst;
+      rewrite lower_bound_aux_equation.
+    - omega.
+    - destruct (P_dec (offset + Nat.div2 (S n'))) as [ HP | ].
+      + apply IHn; eauto.
+        * apply Nat.lt_div2.
+          omega.
+        * specialize (P_monotone2 _ HP).
+          omega.
+      + destruct (le_dec (offset + error) (offset + Nat.div2 (S n'))) as [ Hle | ].
+        * apply P_monotone1 in Hle.
+          congruence.
+        * rewrite IHn with (error0 := error - S (Nat.div2 (S n'))); try omega.
+  Qed.
+
+  Definition lower_bound alpha beta :=
+    lower_bound_aux alpha (beta - alpha).
+
+  Theorem lower_bound_spec alpha beta :
+    alpha <= threshold <= beta ->
+    lower_bound alpha beta = threshold.
+  Proof.
+    unfold lower_bound.
+    intros ?.
+    rewrite lower_bound_aux_spec with (error := threshold - alpha); try omega.
+  Qed.
+End LowerBound.
+
+Section UpperBound.
+  Variable P : nat -> Prop.
+  Variable threshold : nat.
+  Hypothesis P_dec : forall n, { P n } + { ~P n }.
+  Hypothesis P_monotone1 : forall n, n <= threshold -> P n.
+  Hypothesis P_monotone2 : forall n, P n -> n <= threshold.
+  
+  Program Definition upper_bound := lower_bound (fun n => ~P (S n)) _.
+  Next Obligation.
+    destruct (P_dec (S n)); eauto.
+  Qed.
+
+  Lemma upper_bound_spec alpha beta :
+    alpha <= threshold <= beta ->
+    upper_bound alpha beta = threshold.
+  Proof.
+    unfold upper_bound.
+    intros ?.
+    rewrite lower_bound_spec with (threshold := threshold); try omega.
+    - intros n ?.
+      destruct (P_dec (S n)) as [ HP | ].
+      + specialize (P_monotone2 _ HP).
         omega.
-      * specialize (H' _ Heqb).
-        omega.
-    + destruct (le_dec (offset + error) (offset + div2 (S n'))) as [ Hle | ].
-      * apply H in Hle.
-        congruence.
-      * { rewrite IHn with (error := error - S (div2 (S n'))); try omega.
-          - intros ? ?.
-            apply H.
-            omega.
-          - intros ? Hp.
-            specialize (H' _ Hp).
-            omega. }
-Qed.
-
-Definition bsearch p alpha beta :=
-  bsearch_aux p alpha (beta - alpha).
-
-Theorem bsearch_spec : forall p threshold,
-  (forall n, threshold <= n -> p n = true) ->
-  (forall n, p n = true -> threshold <= n) ->
-  forall alpha beta,
-  alpha <= threshold <= beta ->
-  bsearch p alpha beta = threshold.
-Proof.
-  unfold bsearch.
-  intros ? ? H H' alpha beta ?.
-  rewrite bsearch_aux_spec with (error := threshold - alpha); try omega.
-  - intros ? ?.
-    apply H.
-    omega.
-  - intros ? Htrue.
-    specialize (H' _ Htrue).
-    omega.
-Qed.
-
-Definition bsearch' p alpha beta :=
-  bsearch (fun x => negb (p (S x))) alpha beta.
-
-Lemma bsearch'_spec : forall p threshold,
-  (forall n, n <= threshold -> p n = true) ->
-  (forall n, p n = true -> n <= threshold) ->
-  forall alpha beta,
-  alpha <= threshold <= beta ->
-  bsearch' p alpha beta = threshold.
-Proof.
-  unfold bsearch'.
-  intros ? ? H H' ? ? ?.
-  rewrite bsearch_spec with (threshold := threshold); try omega.
-  - intros n ?.
-    remember (p (S n)) as b.
-    symmetry in Heqb.
-    destruct b; simpl.
-    + specialize (H' _ Heqb).
-      omega.
-    + reflexivity.
-  - intros n Hp.
-    destruct (le_dec (S n) threshold) as [ Hle | ].
-    + rewrite (H _ Hle) in *.
-      simpl in Hp.
-      congruence.
-    + omega.
-Qed.
+      + eauto.
+    - intros n Hp.
+      destruct (le_dec (S n) threshold) as [ Hle | ].
+      + exfalso.
+        eauto.
+      + omega.
+  Qed.
+End UpperBound.
 
 (* sqrt 4 *)
 Eval compute in
-  (bsearch
-    (fun n =>
-      if le_dec 4 (n * n) then true
-      else false) 0 10).
+  (lower_bound
+     (fun n => 4 <= n * n)
+     (fun n => match le_dec 4 (n * n) with left H => left H | right H => right H end) 0 10).
 
-Extract Inductive bool => "bool" ["true" "false"].
+Extract Inductive sumbool => "bool" ["true" "false"].
 Extract Inductive nat => int ["0" "succ"] "(fun fO fS n -> if n = 0 then fO () else fS (n-1))".
 
 Extract Constant plus => "( + )".
 Extract Constant minus => "( - )".
-Extract Constant div2 => "(fun x -> x / 2)".
-Extract Constant negb => "not".
-Extraction "bsearch.ml" bsearch bsearch'.
+Extract Constant Nat.div2 => "(fun x -> x / 2)".
+Extraction "bsearch.ml" lower_bound upper_bound.
