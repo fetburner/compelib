9f087958302dbebf9ed1afddebc25f67b04a1aac
fetburner <masayuki2718@yahoo.co.jp>
2017-07-26 01:49:50 +0900

    Implement tournament-like folding

--- misc.ml
+++ misc.ml
@@ -6,3 +6,17 @@ let rec foldn f init = function
 let rec gcd n m =
   if m = 0 then n
   else gcd m (n mod m)
+
+let rec fold_tournament dir f = function
+  | [x] -> x
+  | xs ->
+      List.fold_left (fun (acc, prev) x ->
+        match prev with
+        | None -> (acc, Some x)
+        | Some y -> ((if dir then f y x else f x y) :: acc, None)) ([], None) xs
+      |> (function
+          | (acc, None) -> acc
+          | (acc, Some x) -> x :: acc)
+      |> fold_tournament (not dir) f
+(* fold_tournament ( * ) [x1; x2; x3; x4; x5; x6; x7; x8 ... ] = (... (((x1 * x2) * (x3 * x4)) * ((x5 * x6) * (x7 * x8))) ...) * ( ... ) *)
+let fold_tournament f xs = fold_tournament true f xs
