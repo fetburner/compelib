2eb27b9af8bfd7f94e877f899ed4ce5dc480e04b
fetburner <masayuki2718@yahoo.co.jp>
2018-05-03 17:35:57 +0900

    Modify the interface of topological sort

--- graph/scc.ml
+++ graph/scc.ml
@@ -1,40 +1,50 @@
-module type GRAPH = sig
-  type vertex
-  type vertex_set
-  type edge_set = vertex -> vertex_set
-  type graph = vertex_set * edge_set
-  val sort : graph -> vertex list
-  val scc : graph -> vertex list list
-end
+module DirectedGraph
+  (Vertex : sig
+    type t
+    val compare : t -> t -> int
+  end) :
+sig
+  (* トポロジカルソート *)
+  val sort :
+    (* 頂点のリスト *)
+    Vertex.t list ->
+    (* 隣接リスト *)
+    (Vertex.t -> Vertex.t list) ->
+    Vertex.t list
 
-module Graph (VSet : Set.S) :
-  GRAPH
-  with type vertex = VSet.elt and type vertex_set = VSet.t =
+  (* 強連結成分分解 *)
+  val scc :
+    (* 頂点のリスト *)
+    Vertex.t list ->
+    (* 隣接リスト *)
+    (Vertex.t -> Vertex.t list) ->
+    Vertex.t list list
+end =
 struct
-  type vertex = VSet.elt
-  type vertex_set = VSet.t
-  type edge_set = vertex -> vertex_set
-  type graph = vertex_set * edge_set
+  module VSet = Set.Make (Vertex)
 
   let rec visit es v (vs, l) =
     if VSet.mem v vs then
       let (vs', l') =
-        VSet.fold (visit es) (es v) (VSet.remove v vs, l) in
+        List.fold_right (visit es) (es v) (VSet.remove v vs, l) in
       (vs', v :: l')
     else (vs, l)
 
-  let sort (vs, es) = snd (VSet.fold (visit es) vs (vs, []))
+  let sort vs es =
+    snd (List.fold_right (visit es) vs (VSet.of_list vs, []))
 
-  let scc (vs, es) =
-    sort (vs, es)
+  let scc vs es =
+    sort vs es
     |> List.rev
     |> List.fold_left (fun (vs, l) v ->
         if VSet.mem v vs then 
           let (vs', cs) = visit es v (vs, []) in
           (vs', cs :: l)
-        else (vs, l)) (vs, [])
+        else (vs, l)) (VSet.of_list vs, [])
     |> snd
 end
+
+(* sample code *)
 
 module Int = struct
   type t = int
@@ -42,22 +52,22 @@ module Int = struct
 end
 
 module IntSet = Set.Make (Int)
-module IntG = Graph (IntSet)
+module IntG = DirectedGraph (Int);;
 
-IntG.scc (IntSet.of_list [ 1; 2; 3; 4; 5; 6; 7 ], function
-  | 1 -> IntSet.singleton 2
-  | 2 -> IntSet.singleton 3
-  | 3 -> IntSet.of_list [ 1; 4 ]
-  | 4 -> IntSet.singleton 5
-  | 5 -> IntSet.of_list [ 4; 6; 7 ]
-  | 6 -> IntSet.empty
-  | 7 -> IntSet.empty)
+IntG.scc [1; 2; 3; 4; 5; 6; 7] (function
+  | 1 -> [2]
+  | 2 -> [3]
+  | 3 -> [1; 4]
+  | 4 -> [5]
+  | 5 -> [4; 6; 7]
+  | 6 -> []
+  | 7 -> []);;
 
-IntG.sort (IntSet.of_list [1; 2; 3; 4; 5; 6; 7], function
-  | 1 -> IntSet.of_list [4; 6]
-  | 2 -> IntSet.of_list [5; 7]
-  | 3 -> IntSet.of_list [4; 6]
-  | 4 -> IntSet.empty
-  | 5 -> IntSet.singleton 7
-  | 6 -> IntSet.singleton 5
-  | 7 -> IntSet.empty)
+IntG.sort [1; 2; 3; 4; 5; 6; 7] (function
+  | 1 -> [2]
+  | 2 -> [3]
+  | 3 -> [1; 4]
+  | 4 -> [5]
+  | 5 -> [4; 6; 7]
+  | 6 -> []
+  | 7 -> [])
