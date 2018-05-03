99cd496de9f04e0f438c585419229de8056417cb
fetburner <masayuki2718@yahoo.co.jp>
2018-05-03 16:27:15 +0900

    Implement Warshall-Floyd algorithm

---
+++ graph/warshallFloyd.ml
@@ -0,0 +1,58 @@
+module WeightedDirectedGraph
+  (Weight : sig
+    type t
+    val inf : t
+    val zero : t
+    val min : t -> t -> t
+    val ( + ) : t -> t -> t
+  end) :
+sig
+  val warshall_floyd :
+    (* 頂点のリスト *)
+    'v list ->
+    (* 辺のリスト *)
+    ('v * 'v * Weight.t) list ->
+    ('v -> 'v -> Weight.t)
+end =
+struct
+  let warshall_floyd vs es =
+    (* 準備 *)
+    let d =
+      let n = List.length vs in
+      Hashtbl.create (n * n) in
+    List.iter (fun (u, v, c) ->
+      Hashtbl.replace d (u, v) c) es;
+    List.iter (fun v ->
+      Hashtbl.replace d (v, v) Weight.zero) vs;
+    (* メインループ *)
+    List.iter (fun i ->
+      List.iter (fun j ->
+        List.iter (fun k ->
+          (* d.(j).(k) <- min d.(j).(k) (d.(j).(i) + d.(i).(k)) *)
+          let open Weight in
+          match Hashtbl.find d (j, i) + Hashtbl.find d (i, k) with
+          | exception Not_found -> ()
+          | djk' ->
+              Hashtbl.replace d (j, k) @@
+                try min djk' (Hashtbl.find d (j, k))
+                with Not_found -> djk') vs) vs) vs;
+    fun u v -> try Hashtbl.find d (u, v) with Not_found -> Weight.inf
+end
+
+(* sample code *)
+
+module G = WeightedDirectedGraph (struct
+  type t = float
+  let inf = infinity
+  let zero = 0.
+  let min = min
+  let ( + ) = ( +. )
+end)
+
+let d = G.warshall_floyd [0; 1; 2; 3; 4]
+  [ (0, 1, 4.); (0, 4, 3.);
+    (1, 0, 4.); (1, 2, 2.);
+    (2, 1, 2.); (2, 3, 3.); (2, 4, 2.);
+    (3, 2, 3.); (3, 4, 7.);
+    (4, 0, 3.); (4, 2, 2.); (4, 3, 7.) ]
+Array.init 5 (fun i -> Array.init 5 (d i));;
