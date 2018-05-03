dee70dc164234b3a65cf52de7f4c2044baee213b
fetburner <masayuki2718@yahoo.co.jp>
2018-05-03 18:27:33 +0900

    Meld *.mli into *.ml

--- pUnionFind.ml
+++ pUnionFind.ml
@@ -1,26 +1,35 @@
-type t = { rank : int PArray.t; mutable parent : int PArray.t }
-type class_ = int
+module PUnionFind : sig
+  (* persistent union-find data structure *)
+  type t
+  type class_
 
-let make n = { rank = PArray.make n 0; parent = PArray.init n (fun i -> i) }
+  val make : int -> t
+  val find : t -> int -> class_
+  val union : t -> int -> int -> t
+end = struct
+  type t = { rank : int PArray.t; mutable parent : int PArray.t }
+  type class_ = int
 
-let rec find parent i k =
-  let j = PArray.get parent i in
-  if i = j then k parent i
-  else find parent j (fun parent r -> k (PArray.set parent i r) r)
-let find ({ parent } as h) i = find parent i (fun parent r ->
-  h.parent <- parent;
-  r)
+  let make n = { rank = PArray.make n 0; parent = PArray.init n (fun i -> i) }
 
-let union uf x y =
-  let cx = find uf x in
-  let cy = find uf y in
-  if cx = cy then uf
-  else begin
-    let rx = PArray.get uf.rank x in
-    let ry = PArray.get uf.rank y in
-    match compare rx ry with
-    | -1 -> { uf with parent = PArray.set uf.parent cx cy }
-    | 1 -> { uf with parent = PArray.set uf.parent cy cx }
-    | 0 -> { rank = PArray.set uf.rank cx (cx + 1); parent = PArray.set uf.parent cy cx }
-  end
+  let rec find parent i k =
+    let j = PArray.get parent i in
+    if i = j then k parent i
+    else find parent j (fun parent r -> k (PArray.set parent i r) r)
+  let find ({ parent } as h) i = find parent i (fun parent r ->
+    h.parent <- parent;
+    r)
 
+  let union uf x y =
+    let cx = find uf x in
+    let cy = find uf y in
+    if cx = cy then uf
+    else begin
+      let rx = PArray.get uf.rank x in
+      let ry = PArray.get uf.rank y in
+      match compare rx ry with
+      | -1 -> { uf with parent = PArray.set uf.parent cx cy }
+      | 1 -> { uf with parent = PArray.set uf.parent cy cx }
+      | 0 -> { rank = PArray.set uf.rank cx (cx + 1); parent = PArray.set uf.parent cy cx }
+    end
+end
--- pUnionFind.mli
+++
@@ -1,7 +0,0 @@
-(* persistent union-find data structure *)
-type t
-type class_
-
-val make : int -> t
-val find : t -> int -> class_
-val union : t -> int -> int -> t
