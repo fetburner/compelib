8be01e9eb007cf71ee697aa55d1c9a958a5324bc
fetburner <masayuki2718@yahoo.co.jp>
2018-05-04 14:07:51 +0900

    Implement an array variant of Bellman-Ford algorithm

--- graph/bellmanFord.ml
+++ graph/bellmanFord.ml
@@ -6,6 +6,17 @@ module WeightedDirectedGraph
     val compare : t -> t -> int
   end) :
 sig
+  (* 配列版の実装 *)
+  val raw_bellman_ford :
+    (* 頂点数n *)
+    int ->
+    (* 辺のリスト *)
+    (* 頂点は0からn-1までの整数でなくてはならない *)
+    (int * int * Weight.t) list ->
+    (* 始点 *)
+    int ->
+    (int -> [ `Weight of Weight.t | `Inf | `NegInf ])
+
   val bellman_ford :
     (* 頂点数 *)
     int ->
@@ -16,6 +27,32 @@ sig
     ('v -> [ `Weight of Weight.t | `Inf | `NegInf ])
 end =
 struct
+  let raw_bellman_ford n es s =
+    (* 距離を覚えるやつ *)
+    (* Noneで無限を表す *)
+    let d = Array.make n None in
+    (* 経路に負閉路が含まれる頂点を覚えるやつ *)
+    let neg = Array.make n false in
+    d.(s) <- Some Weight.zero;
+    for i = 0 to 2 * n - 1 do
+      List.iter (fun (u, v, c) ->
+        (* c は u から v への辺の重さ *)
+        (* d.(v) <- min d.(v) (d.(u) + c) *)
+        let open Weight in
+        match d.(v), d.(u) with
+        | _, None -> () (* d.(u) は無限大 *)
+        (* d.(v) <= d.(u) + c *)
+        | Some dv, Some du when compare dv (du + c) <= 0 -> ()
+        | _, Some du ->
+            d.(v) <- Some (du + c);
+              (* n 回目以降に変更が起こった場合，v までの経路に負閉路が含まれている *)
+              if n - 1 <= i then
+                neg.(v) <- true) es
+    done;
+    fun v ->
+      if neg.(v) then `NegInf
+      else match d.(v) with None -> `Inf | Some d -> `Weight d
+
   let bellman_ford n es s =
     (* 距離を覚えるやつ *)
     (* d に入っていない頂点への距離は無限大とみなす *)
@@ -56,6 +93,18 @@ module G = WeightedDirectedGraph (struct
   let ( + ) = ( +. )
   let compare = compare
 end)
+
+let d = G.raw_bellman_ford 8
+  [ (1, 2, 1.); (2, 3, 1.); (3, 7, 1.); (4, 5, -1.); (5, 6, -1.); (6, 4, -1.) ] 1;;
+Array.init 8 d;;
+
+let d = G.raw_bellman_ford 8
+  [ (1, 2, 1.); (2, 3, 1.); (3, 7, 1.); (4, 5, -1.); (5, 6, -1.); (6, 4, -1.); (7, 4, -1.) ] 1;;
+Array.init 8 d;;
+
+let d = G.raw_bellman_ford 8
+  [ (1, 2, 1.); (2, 3, 1.); (3, 7, 1.); (4, 5, -1.); (5, 6, -1.); (6, 4, -1.); (7, 4, -1.); (4, 7, 1.) ] 1;;
+Array.init 8 d;;
 
 let d = G.bellman_ford 7
   [ (1, 2, 1.); (2, 3, 1.); (3, 7, 1.); (4, 5, -1.); (5, 6, -1.); (6, 4, -1.) ] 1;;
