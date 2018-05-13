0602432958d823353e4b62246f176f65f54e41cd
fetburner <masayuki2718@yahoo.co.jp>
2018-05-14 02:57:42 +0900

    Improve the implementation of Dijkstra algorithm

--- graph/dijkstra.ml
+++ graph/dijkstra.ml
@@ -27,45 +27,68 @@ struct
   let rec dijkstra_aux e (d, q) =
     match WMap.min_binding q with
     | exception Not_found -> d
-    | (w, vs) ->
-        dijkstra_aux e @@ List.fold_right (fun v ->
-          (* w = d.(v) *)
-          List.fold_right (fun (u, c) (d, q) ->
-            (* c は頂点 v から頂点 u への辺の重み *)
+    | (w, us) ->
+        dijkstra_aux e @@ List.fold_left (fun (d, q) u ->
+          if Weight.compare (VMap.find u d) w < 0
+          (* 既に頂点uを訪れていた *)
+          then (d, q)
+          (* w = d.(u) *)
+          else List.fold_left (fun (d, q) (v, c) ->
             let open Weight in
             if
-              (* d.(u) <= d.(v) + c *)
-              try 0 <= compare (w + c) (VMap.find u d)
+              (* d.(v) <= d.(u) + c *)
+              try Weight.compare (VMap.find v d) (w + c) <= 0
               with Not_found -> false (* d.(u) は無限大 *)
             then (d, q)
             else
-              VMap.add u (w + c) d,
-              WMap.add (w + c) (u :: try WMap.find (w + c) q with Not_found -> []) q)
-          (e v)) vs (d, WMap.remove w q)
+              VMap.add v (w + c) d,
+              WMap.add (w + c) (v :: try WMap.find (w + c) q with Not_found -> []) q)
+          (d, q) (e u)) (d, WMap.remove w q) us
 
-  let rec dijkstra e s =
+  let dijkstra e s =
     let d =
       dijkstra_aux e
         (VMap.singleton s Weight.zero, WMap.singleton Weight.zero [s]) in
-    fun v -> try Some (VMap.find v d) with Not_found -> None
+    fun t -> try Some (VMap.find t d) with Not_found -> None
 end
 
 (* sample code *)
 
 module Int = struct
   type t = int
-  let zero = 0
-  let ( + ) = ( + )
   let compare = compare
 end
 
-module G = WeightedDirectedGraph (Int) (Int)
+module G = WeightedDirectedGraph (Int)
+  (struct
+    type t = float
+    let zero = 0.
+    let ( + ) = ( +. )
+    let compare = compare
+  end)
 
-let d = G.dijkstra (function
-  | 0 -> [ (1, 7); (2, 9); (5, 14) ]
-  | 1 -> [ (0, 7); (2, 10); (3, 15) ]
-  | 2 -> [ (0, 9); (1, 10); (3, 11); (5, 2) ]
-  | 3 -> [ (1, 15); (2, 11); (4, 6) ]
-  | 4 -> [ (3, 6); (5, 9) ]
-  | 5 -> [ (0, 14); (2, 2); (4, 9) ]) 0;;
-Array.init 7 d
+let e =
+  [|[ (1, 7.); (2, 9.); (5, 14.) ];
+    [ (0, 7.); (2, 10.); (3, 15.) ];
+    [ (0, 9.); (1, 10.); (3, 11.); (5, 2.) ];
+    [ (1, 15.); (2, 11.); (4, 6.) ];
+    [ (3, 6.); (5, 9.) ];
+    [ (0, 14.); (2, 2.); (4, 9.) ]|];;
+
+Array.init 7 (G.dijkstra (fun u -> e.(u)) 0);;
+
+module G' = WeightedDirectedGraph (Int)
+  (struct
+    type t = float * (string list -> string list)
+    let zero = (0., fun xs -> xs)
+    let ( + ) (c, f) (d, g) = (c +. d, fun xs -> f (g xs))
+    let compare (c, _) (d, _) = compare c d
+  end)
+
+let e' =
+  Array.mapi (fun u ->
+    List.map (fun (v, c) ->
+      let s = Printf.sprintf "%d->%d" u v in
+      (v, (c, fun xs -> s :: xs)))) e;;
+
+Array.map (fun (Some (c, f)) -> (c, f [])) @@ Array.init 6 (G'.dijkstra (fun u -> e'.(u)) 0);;
