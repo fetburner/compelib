13c365122c5455a5e3fdee8c9c5a6c0c597e3147
fetburner <masayuki2718@yahoo.co.jp>
2018-05-15 01:02:20 +0900

    Wrote another implementation of persistent union-find data structure

--- unionfind/pUnionFind.ml
+++ unionfind/pUnionFind.ml
@@ -1,8 +1,13 @@
-(* 永続ハッシュテーブルを用いた永続素集合データ構造 *)
-module PUnionFind
-  (Index : sig
-    type t
-    val compare : t -> t -> int
+(* 永続素集合データ構造 *)
+module PUnionFindFn
+  (* 永続ハッシュテーブルの実装 *)
+  (PHashtbl : sig
+    type 'a t
+    type key
+    val create : int -> 'a t
+    val find : 'a t -> key -> 'a
+    val replace : 'a t -> key -> 'a -> 'a t
+    val compare_key : key -> key -> int
   end) :
 sig
   type t
@@ -10,22 +15,22 @@ sig
 
   (* n要素の永続素集合データ構造を作るが，ハッシュなのでn要素以上も入れられる *)
   val make : int -> t
-  val find : t -> Index.t -> class_
-  val union : t -> Index.t -> Index.t -> t
+  val find : t -> PHashtbl.key -> class_
+  val union : t -> PHashtbl.key -> PHashtbl.key -> t
 
   val compare_class : class_ -> class_ -> int
 end =
 struct
   type t =
-    { rank : (Index.t, int) PHashtbl.t;
-      mutable parent : (Index.t, Index.t) PHashtbl.t }
-  type class_ = Index.t
+    { rank : int PHashtbl.t;
+      mutable parent : PHashtbl.key PHashtbl.t }
+  type class_ = PHashtbl.key
 
   let make n = { rank = PHashtbl.create n; parent = PHashtbl.create n }
 
   let rec find parent i k =
     let j = try PHashtbl.find parent i with Not_found -> i in
-    if Index.compare i j = 0 then k parent i
+    if PHashtbl.compare_key i j = 0 then k parent i
     else find parent j (fun parent r -> k (PHashtbl.replace parent i r) r)
   let find ({ parent } as h) i =
     find parent i (fun parent r -> h.parent <- parent; r)
@@ -33,16 +38,50 @@ struct
   let union uf x y =
     let cx = find uf x in
     let cy = find uf y in
-    if Index.compare cx cy = 0 then uf
+    if PHashtbl.compare_key cx cy = 0 then uf
     else begin
       let rx = try PHashtbl.find uf.rank x with Not_found -> 0 in
       let ry = try PHashtbl.find uf.rank y with Not_found -> 0 in
-      if rx < ry then
-        { uf with parent = PHashtbl.replace uf.parent cx cy }
-      else
-        { parent = PHashtbl.replace uf.parent cy cx;
-          rank = PHashtbl.replace uf.rank cx (rx + 1) }
+      match compare rx ry with
+      | -1 -> { uf with parent = PHashtbl.replace uf.parent cx cy }
+      | 1 ->  { uf with parent = PHashtbl.replace uf.parent cy cx }
+      | 0 ->
+          { parent = PHashtbl.replace uf.parent cy cx;
+            rank = PHashtbl.replace uf.rank cx (rx + 1) }
     end
 
-  let compare_class = Index.compare
+  let compare_class = PHashtbl.compare_key
 end
+
+(* 永続ハッシュテーブルによる素集合データ構造 *)
+module PUnionFindHashtbl
+  (Index : sig
+    type t
+    val compare : t -> t -> int
+  end)
+= PUnionFindFn
+  (struct
+    type 'a t = (Index.t, 'a) PHashtbl.t
+    type key = Index.t
+    let create n = PHashtbl.create n
+    let find = PHashtbl.find
+    let replace = PHashtbl.replace
+    let compare_key = Index.compare
+  end)
+
+(* Mapによる素集合データ構造 *)
+module PUnionFindMap
+  (Index : sig
+    type t
+    val compare : t -> t -> int
+  end)
+= PUnionFindFn
+  (struct
+    module IMap = Map.Make (Index)
+    type 'a t = 'a IMap.t
+    type key = Index.t
+    let create _ = IMap.empty
+    let find m i = IMap.find i m
+    let replace m i x = IMap.add i x m
+    let compare_key = Index.compare
+  end)
