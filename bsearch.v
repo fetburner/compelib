daf11a78a48dbd91fb9192f3be1df1a4c6561a6e
fetburner <masayuki2718@yahoo.co.jp>
2018-07-06 11:26:56 +0900

    Use integer for binary search

--- bsearch.v
+++ bsearch.v
@@ -1,8 +1,10 @@
-Require Import Bool Arith Omega Program.
+Require Import Bool ZArith Omega Program.
+
+Open Scope Z_scope.
 
 Program Fixpoint upper_bound' l r
   (p : forall n, l <= n < r -> bool)
-  (Hacc : @Acc _ (ltof _ (fun p => snd p - fst p)) (l, r))
+  (Hacc : @Acc _ (ltof _ (fun p => Z.to_nat (snd p - fst p))) (l, r))
   (_ : exists m,
     l <= m < r /\
     (forall n H, p n H = true -> n <= m) /\
@@ -12,7 +14,7 @@ Program Fixpoint upper_bound' l r
     (forall n H, n <= m -> p n H = true) } :=
   match Hacc with
   | Acc_intro _ Hacc' =>
-      if le_gt_dec r (S l)
+      if Z_le_gt_dec r (Z.succ l)
       then exist _ l _
       else
         let m := (l + r) / 2 in
@@ -30,48 +32,52 @@ Next Obligation.
 Qed.
 Next Obligation.
   split.
-  - apply (Nat.div_le_lower_bound _ 2); omega.
-  - apply (Nat.div_lt_upper_bound _ 2); omega.
+  - apply (Z.div_le_lower_bound _ 2); omega.
+  - apply (Z.div_lt_upper_bound _ 2); omega.
 Qed.
 Next Obligation.
   assert ((l + r) / 2 < r).
-  { apply Nat.div_lt_upper_bound; omega. }
-  unfold ltof. simpl in *. omega.
+  { apply Z.div_lt_upper_bound; omega. }
+  assert (l <= (l + r) / 2).
+  { apply Z.div_le_lower_bound; omega. }
+  unfold ltof. simpl in *. apply Z2Nat.inj_lt; omega.
 Qed.
 Next Obligation.
   assert ((l + r) / 2 < r).
-  { apply Nat.div_lt_upper_bound; omega. }
+  { apply Z.div_lt_upper_bound; omega. }
   simpl in *. omega.
 Qed.
 Next Obligation.
   replace n with H; eauto.
-  assert (n <= H) by eapply l0, H4, le_n.
-  assert (H <= n) by eapply H3, e, le_n.
+  assert (n <= H) by eapply l0, H4, Z.le_refl.
+  assert (H <= n) by eapply H3, e, Z.le_refl.
   omega.
   Unshelve.
   - eauto.
-  - destruct (le_gt_dec ((l + r) / 2) H) as [ Hle | ]; simpl in *; try omega.
+  - destruct (Z_le_gt_dec ((l + r) / 2) H) as [ Hle | ]; simpl in *; try omega.
     generalize H1. erewrite (e _ _ Hle). inversion 1.
 Qed.
 Next Obligation.
   exists H. repeat split; eauto.
-  destruct (le_gt_dec ((l + r) / 2) H) as [ Hle | ]; simpl in *; try omega.
+  destruct (Z_le_gt_dec ((l + r) / 2) H) as [ Hle | ]; simpl in *; try omega.
   generalize H1. erewrite (e _ _ Hle). inversion 1.
 Qed.
 Next Obligation.
-  assert (l < (l + r) / 2).
-  { apply Nat.div_le_lower_bound; omega. }
-  unfold ltof. simpl in *. omega.
+  assert (Z.succ l <= (l + r) / 2).
+  { apply Z.div_le_lower_bound; omega. }
+  assert ((l + r) / 2 <= r).
+  { apply Z.div_le_upper_bound; omega. }
+  unfold ltof. simpl in *. apply Z2Nat.inj_lt; omega.
 Qed.
 Next Obligation.
-  assert (l < (l + r) / 2).
-  { apply Nat.div_le_lower_bound; omega. }
+  assert (Z.succ l <= (l + r) / 2).
+  { apply Z.div_le_lower_bound; omega. }
   unfold ltof. simpl in *. omega.
 Qed.
 Next Obligation.
   replace n with H; eauto.
-  assert (H <= n) by eapply H3, e, le_n.
-  assert (n <= H) by eapply l0, H4, le_n.
+  assert (H <= n) by eapply H3, e, Z.le_refl.
+  assert (n <= H) by eapply l0, H4, Z.le_refl.
   omega.
   Unshelve.
   - eauto.
@@ -86,7 +92,7 @@ Definition upper_bound l r p :=
 
 Program Fixpoint lower_bound' l r
   (p : forall n, l < n <= r -> bool)
-  (Hacc : @Acc _ (ltof _ (fun p => snd p - fst p)) (l, r))
+  (Hacc : @Acc _ (ltof _ (fun p => Z.to_nat (snd p - fst p))) (l, r))
   (_ : exists m,
     l < m <= r /\
     (forall n H, p n H = true -> m <= n) /\
@@ -96,7 +102,7 @@ Program Fixpoint lower_bound' l r
     (forall n H, m <= n -> p n H = true) } :=
   match Hacc with
   | Acc_intro _ Hacc' =>
-      if le_gt_dec r (S l)
+      if Z_le_gt_dec r (Z.succ l)
       then exist _ r _
       else
         let m := (l + r) / 2 in
@@ -114,48 +120,52 @@ Next Obligation.
 Qed.
 Next Obligation.
   split.
-  - apply (Nat.div_le_lower_bound _ 2); omega.
-  - apply (Nat.div_le_upper_bound _ 2); omega.
+  - assert (Z.succ l <= (l + r) / 2) by (apply (Z.div_le_lower_bound _ 2); omega). omega.
+  - apply (Z.div_le_upper_bound _ 2); omega.
 Qed.
 Next Obligation.
-  assert (l < (l + r) / 2).
-  { apply Nat.div_le_lower_bound; omega. }
-  unfold ltof. simpl in *. omega.
+  assert ((l + r) / 2 <= r).
+  { apply Z.div_le_upper_bound; omega. }
+  assert (Z.succ l <= (l + r) / 2).
+  { apply Z.div_le_lower_bound; omega. }
+  unfold ltof. simpl in *. apply Z2Nat.inj_lt; omega.
 Qed.
 Next Obligation.
-  assert (l < (l + r) / 2).
-  { apply Nat.div_le_lower_bound; omega. }
+  assert (Z.succ l <= (l + r) / 2).
+  { apply Z.div_le_lower_bound; omega. }
   simpl in *. omega.
 Qed.
 Next Obligation.
   replace n with H; eauto.
-  assert (n <= H) by eapply H3, e, le_n.
-  assert (H <= n) by eapply l0, H4, le_n.
+  assert (n <= H) by eapply H3, e, Z.le_refl.
+  assert (H <= n) by eapply l0, H4, Z.le_refl.
   omega.
   Unshelve.
-  - destruct (le_gt_dec H ((l + r) / 2)) as [ Hle | ]; simpl in *; try omega.
+  - destruct (Z_le_gt_dec H ((l + r) / 2)) as [ Hle | ]; simpl in *; try omega.
     generalize H1. erewrite (e _ _ Hle). inversion 1.
   - eauto.
 Qed.
 Next Obligation.
   exists H. repeat split; eauto.
-  destruct (le_gt_dec H ((l + r) / 2)) as [ Hle | ]; simpl in *; try omega.
+  destruct (Z_le_gt_dec H ((l + r) / 2)) as [ Hle | ]; simpl in *; try omega.
   generalize H1. erewrite (e _ _ Hle). inversion 1.
 Qed.
 Next Obligation.
+  assert (l <= (l + r) / 2).
+  { apply Z.div_le_lower_bound; omega. }
   assert ((l + r) / 2 < r).
-  { apply Nat.div_lt_upper_bound; omega. }
-  unfold ltof. simpl in *. omega.
+  { apply Z.div_lt_upper_bound; omega. }
+  unfold ltof. simpl in *. apply Z2Nat.inj_lt; omega.
 Qed.
 Next Obligation.
   assert ((l + r) / 2 < r).
-  { apply Nat.div_lt_upper_bound; omega. }
+  { apply Z.div_lt_upper_bound; omega. }
   unfold ltof. simpl in *. omega.
 Qed.
 Next Obligation.
   replace n with H; eauto.
-  assert (H <= n) by eapply l0, H4, le_n.
-  assert (n <= H) by eapply H3, e, le_n.
+  assert (H <= n) by eapply l0, H4, Z.le_refl.
+  assert (n <= H) by eapply H3, e, Z.le_refl.
   omega.
   Unshelve.
   - eauto.
@@ -170,10 +180,11 @@ Definition lower_bound l r p :=
 
 Extract Inductive bool => "bool" ["true" "false"].
 Extract Inductive sumbool => "bool" ["true" "false"].
-Extract Inductive nat => int ["0" "succ"] "(fun fO fS n -> if n = 0 then fO () else fS (n-1))".
+Extract Inductive positive => int ["(fun n -> 2 * n + 1)" "(( * ) 2)" "1"] "(fun fI fO f1 n -> if n = 1 then f1 () else if n mod 2 = 0 then fO (n / 2) else fI (n / 2))".
+Extract Inductive Z => int ["0" "" "( ~- )"] "(fun fO fpos fneg n -> if n = 0 then fO () else if 0 < n then fpos n else fneg (~-n))".
 
-Extract Constant plus => "( + )".
-Extract Constant minus => "( - )".
-Extract Constant Nat.div => "( / )".
-Extract Constant le_gt_dec => "( <= )".
+Extract Constant Z.add => "( + )".
+Extract Constant Z.sub => "( - )".
+Extract Constant Z.div => "( / )".
+Extract Constant Z_le_gt_dec => "( <= )".
 Extraction "bsearch.ml" lower_bound upper_bound.
