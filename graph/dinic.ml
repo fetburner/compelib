3fce34e45d29c02c63e08c43378fc3b19b37e74c
fetburner <masayuki2718@yahoo.co.jp>
2018-07-15 04:57:27 +0900

    Modify the implementation of Dinic's algorithm

--- graph/dinic.ml
+++ graph/dinic.ml
@@ -12,8 +12,8 @@ sig
   val max_flow :
     (* 頂点の数（Hashtblを用いるので目安程度） *)
     int ->
-    (* 各辺の容量 逆辺を含める必要はない *)
-    ('v * 'v * Flow.t) list ->
+    (* 各辺とその逆辺の容量 *)
+    ('v * 'v * Flow.t * Flow.t) list ->
     (* 始点 *)
     'v ->
     (* 終点 *)
@@ -35,22 +35,21 @@ struct
   let rec dfs capacity add_edge iter level v t f =
     if v = t then f
     else begin
-      let rec loop () =
+      let rec find () =
         match Hashtbl.find iter v with
         | exception Not_found -> Flow.zero
         | (u, i) ->
             Hashtbl.remove iter v;
             (* capacity i <= 0 *)
-            if Flow.compare (capacity i) Flow.zero <= 0 || level u <= level v then loop ()
+            if Flow.compare (capacity i) Flow.zero <= 0 || level u <= level v then find ()
             else begin
               let d = dfs capacity add_edge iter level u t @@
                 (* min f (capacity i) *)
                 if Flow.compare f (capacity i) <= 0 then f else capacity i in
               (* d <= 0 *)
-              if Flow.compare d Flow.zero <= 0 then loop ()
+              if Flow.compare d Flow.zero <= 0 then find ()
               else (add_edge i d; d)
-            end in
-      loop ()
+            end in find ()
     end
 
   let max_flow n es s t =
@@ -59,27 +58,25 @@ struct
     let capacity = Array.make (2 * List.length es) Flow.zero in
     (* 逆辺を張りつつ，隣接リスト形式に変換 *)
     let adj = Hashtbl.create n in
-    List.iteri (fun i (u, v, c) ->
+    List.iteri (fun i (u, v, c, c') ->
       (* 各辺は番号で管理する *)
       Hashtbl.add adj u (v, 2 * i);
       (* 容量はcapacityを見てほしい *)
       capacity.(2 * i) <- c;
       (* 逆辺 *)
       Hashtbl.add adj v (u, 2 * i + 1);
-      capacity.(2 * i + 1) <- Flow.zero) es;
+      capacity.(2 * i + 1) <- c') es;
     (* 番号iの辺にcだけフローを流す *)
     let add_edge i c =
       let open Flow in
       capacity.(i) <- capacity.(i) - c;
       (* 逆辺 *)
       capacity.(i lxor 1) <- capacity.(i lxor 1) + c in
-    let rec loop flow =
+    let rec outer flow =
       let level = G.bfs n (fun v ->
-        List.map (fun (u, _) -> (u, ())) @@
-        List.filter (fun (_, i) ->
-          (* 0 < capacity.(i) *)
-          Flow.compare Flow.zero capacity.(i) < 0) @@
-          Hashtbl.find_all adj v) s in
+        List.concat @@ List.map (fun (u, i) ->
+          if Flow.compare capacity.(i) Flow.zero <= 0 then []
+          else [(u, ())]) @@ Hashtbl.find_all adj v) s in
       if level t = None then flow
       else
         let iter = Hashtbl.copy adj in
@@ -87,9 +84,10 @@ struct
           let f = dfs (fun i -> capacity.(i)) add_edge iter level s t Flow.inf in
           if Flow.compare f Flow.zero <= 0 then flow
           else inner (Flow.( + ) flow f) in
-        loop @@ inner flow in
-    let f = loop Flow.zero in
-    (f, List.mapi (fun i (u, v, _) -> (u, v, capacity.(2 * i + 1))) es)
+        outer @@ inner flow in
+    let f = outer Flow.zero in
+    (f, List.mapi (fun i (u, v, c, _) ->
+      (u, v, Flow.( - ) c capacity.(2 * i))) es)
 end
 
 (* 蟻本p. 188のグラフで試す *)
@@ -103,5 +101,5 @@ module G = FlowNetwork (struct
 end);;
 
 G.max_flow 5
-  [ (0, 1, 10); (0, 2, 2); (1, 2, 6); (1, 3, 6);
-    (3, 2, 3); (3, 4, 8); (2, 4, 5) ] 0 4;;
+  [ (0, 1, 10, 0); (0, 2, 2, 0); (1, 2, 6, 0); (1, 3, 6, 0);
+    (3, 2, 3, 0); (3, 4, 8, 0); (2, 4, 5, 0) ] 0 4;;
