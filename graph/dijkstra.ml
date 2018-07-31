cd43c37648233adcce7693ee155f332fe4858b60
fetburner <masayuki2718@yahoo.co.jp>
2018-07-31 21:52:55 +0900

    Now, the implementation of Dijkstra's algorithm tolerate infinite graphs!

--- graph/dijkstra.ml
+++ graph/dijkstra.ml
@@ -22,34 +22,41 @@ struct
   module WMap = Map.Make (Weight)
   module VMap = Map.Make (Vertex)
 
-  (* ダイクストラ法のメインループ *)
-  (* d に入っていない頂点への距離は無限大とみなす *)
-  let rec dijkstra_aux e (d, q) =
-    match WMap.min_binding q with
-    | exception Not_found -> d
-    | (w, us) ->
-        dijkstra_aux e @@ List.fold_left (fun (d, q) u ->
-          if Weight.compare (VMap.find u d) w < 0
-          (* 既に頂点uを訪れていた *)
-          then (d, q)
-          (* w = d.(u) *)
-          else List.fold_left (fun (d, q) (v, c) ->
-            let open Weight in
-            if
-              (* d.(v) <= d.(u) + c *)
-              try Weight.compare (VMap.find v d) (w + c) <= 0
-              with Not_found -> false (* d.(v) は無限大 *)
-            then (d, q)
-            else
-              VMap.add v (w + c) d,
-              WMap.add (w + c) (v :: try WMap.find (w + c) q with Not_found -> []) q)
-          (d, q) (e u)) (d, WMap.remove w q) us
-
   let dijkstra e s =
-    let d =
-      dijkstra_aux e
-        (VMap.singleton s Weight.zero, WMap.singleton Weight.zero [s]) in
-    fun t -> try Some (VMap.find t d) with Not_found -> None
+    (* 始点sからの距離 *)
+    (* d に入っていない頂点への距離は無限大とみなす *)
+    let d = ref @@ VMap.singleton s Weight.zero in
+    (* 優先度付きキュー *)
+    let q = ref @@ WMap.singleton Weight.zero [s] in
+    (* ダイクストラ法のメインループ *)
+    let rec dijkstra_aux t =
+      match WMap.min_binding !q with
+      | exception Not_found ->
+          (try Some (VMap.find t !d) with Not_found -> None)
+      | (w, us) ->
+          if
+            (* 現時点で終点までの距離が分かっているか *)
+            try Weight.compare (VMap.find t !d) w <= 0
+            with Not_found -> false
+          (* 既に終点までの距離が分かっているので返す *)
+          then Some (VMap.find t !d)
+          else begin
+            (* 終点までの距離が分かっていないので，ダイクストラ法を続行 *)
+            q := WMap.remove w !q;
+            List.iter (fun u ->
+              if 0 <= Weight.compare (VMap.find u !d) w then
+                (* 未だ頂点uを訪れていない *)
+                List.iter (fun (v, c) ->
+                  let open Weight in
+                  if
+                    try 0 < Weight.compare (VMap.find v !d) (w + c)
+                    with Not_found -> true  (* d.(v) は無限大 *)
+                  then begin
+                    d := VMap.add v (w + c) !d;
+                    q := WMap.add (w + c) (v :: try WMap.find (w + c) !q with Not_found -> []) !q
+                  end) (e u)) us;
+            dijkstra_aux t
+          end in dijkstra_aux
 end
 
 (* sample code *)
@@ -59,13 +66,14 @@ module Int = struct
   let compare = compare
 end
 
-module G = WeightedDirectedGraph (Int)
-  (struct
-    type t = float
-    let zero = 0.
-    let ( + ) = ( +. )
-    let compare = compare
-  end)
+module Float = struct
+  type t = float
+  let zero = 0.
+  let ( + ) = ( +. )
+  let compare = compare
+end
+
+module G = WeightedDirectedGraph (Int) (Float)
 
 let e =
   [|[ (1, 7.); (2, 9.); (5, 14.) ];
@@ -92,3 +100,18 @@ let e' =
       (v, (c, fun xs -> s :: xs)))) e;;
 
 Array.map (fun (Some (c, f)) -> (c, f [])) @@ Array.init 6 (G'.dijkstra (fun u -> e'.(u)) 0);;
+
+(* 無限グラフ!!! *)
+module IntPair = struct
+  type t = int * int
+  let compare = compare
+end
+
+module G = WeightedDirectedGraph (IntPair) (Float)
+
+let d = G.dijkstra
+  (fun (x, y) ->
+    List.map (fun v -> (v, float_of_int (abs x + abs y)))
+    [(x + 1, y); (x - 1, y); (x, y + 1); (x, y - 1)]) (0, 0);;
+Array.init 10 @@ fun x ->
+  Array.init 10 @@ fun y -> d (x, y);;
