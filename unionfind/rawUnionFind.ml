9df528156ffd416024491231007cc144eb3da66f
fetburner <masayuki2718@yahoo.co.jp>
2018-09-30 11:29:43 +0900

    Modify interface

--- unionfind/rawUnionFind.ml
+++ unionfind/rawUnionFind.ml
@@ -1,24 +1,32 @@
 (* 配列版素集合データ構造 *)
 module RawUnionFind : sig
   type t
-  type class_
+
+  module Class : sig
+    type t
+    val to_int : t -> int
+    val compare : t -> t -> int
+  end
 
   (* n要素の素集合データ構造を作る *)
   val make : int -> t
   (* 要素がどの集合に属するか調べる *)
   (* 要素は0からn-1の整数である必要がある *)
-  val find : t -> int -> class_
+  val find : t -> int -> Class.t
   (* 与えられた要素が属する集合同士を合併する *)
   val unite : t -> int -> int -> unit
-
-  val compare_class : class_ -> class_ -> int
 end = struct
   type t = { rank : int array; parent : int array }
-  type class_ = int
+
+  module Class = struct
+    type t = int
+    let to_int c = c
+    let compare = compare
+  end
 
   let make n =
     { rank = Array.make n 0;
-      parent = Array.init n (fun c -> c) }
+      parent = Array.init n @@ fun c -> c }
 
   let rec find uf x =
     if x = uf.parent.(x) then x
@@ -38,7 +46,5 @@ end = struct
           uf.rank.(x) <- 1 + uf.rank.(x)
       end
     end
-
-  let compare_class = compare
 end
 
