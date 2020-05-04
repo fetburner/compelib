8b6e1f1dc32a2fd381d6b58a54f8ab4795d72e8a
fetburner <masayuki2718@yahoo.co.jp>
2020-05-04 20:04:41 +0900

    Improve the performance

--- comb.ml
+++ comb.ml
@@ -1,18 +1,20 @@
 let rec comb acc xs n ys =
   match n, ys with
-  | 0, _ -> List.rev xs :: acc
+  | 0, _ -> xs :: acc
   | _, [] -> acc
   | n, y :: ys -> comb (comb acc xs n ys) (y :: xs) (n - 1) ys
 (* 与えられたリストからn要素を選ぶ組み合わせを列挙 *)
-let comb : int -> 'a list -> 'a list list = fun n xs -> comb [] [] n xs
+let comb : int -> 'a list -> 'a list list = fun n xs ->
+  List.rev @@ comb [] [] n @@ List.rev xs
 
 let rec repcomb acc xs n ys =
   match n, ys with
-  | 0, _ -> List.rev xs :: acc
+  | 0, _ -> xs :: acc
   | _, [] -> acc
   | n, y :: ys' -> repcomb (repcomb acc xs n ys') (y :: xs) (n - 1) ys
 (* 与えられたリストから重複を許してn要素を選ぶ組み合わせを列挙 *)
-let repcomb : int -> 'a list -> 'a list list = fun n xs -> repcomb [] [] n xs
+let repcomb : int -> 'a list -> 'a list list = fun n xs ->
+  List.rev @@ repcomb [] [] n @@ List.rev xs
 
 (* sample code *)
 comb 2 [1; 2; 3];;
