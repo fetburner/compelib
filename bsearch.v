d62ab207628566d2171d772dbf6894261375c221
fetburner <masayuki2718@yahoo.co.jp>
2020-05-05 00:20:17 +0900

    Refactor the formal verification of binary search

--- bsearch.v
+++ bsearch.v
@@ -1,4 +1,4 @@
-Require Import Bool ZArith Omega Program.
+Require Import Bool ZArith Lia Program.
 
 Open Scope Z_scope.
 
@@ -26,66 +26,61 @@ Program Fixpoint upper_bound' l r
             let (n, _) := upper_bound' l m (fun n _ => p n _) (Hacc' _ _) _ in
             exist _ n _ ) eq_refl
   end.
-Next Obligation.
-  repeat split; intros; try omega.
-  eapply H3. omega.
-Qed.
+Next Obligation. auto with zarith. Qed.
 Next Obligation.
   split.
-  - apply (Z.div_le_lower_bound _ 2); omega.
-  - apply (Z.div_lt_upper_bound _ 2); omega.
+  - apply (Z.div_le_lower_bound _ 2); lia.
+  - apply (Z.div_lt_upper_bound _ 2); lia.
 Qed.
 Next Obligation.
   assert ((l + r) / 2 < r).
-  { apply Z.div_lt_upper_bound; omega. }
+  { apply Z.div_lt_upper_bound; lia. }
   assert (l <= (l + r) / 2).
-  { apply Z.div_le_lower_bound; omega. }
-  unfold ltof. simpl in *. apply Z2Nat.inj_lt; omega.
+  { apply Z.div_le_lower_bound; lia. }
+  apply Z2Nat.inj_lt; simpl; lia.
 Qed.
 Next Obligation.
   assert ((l + r) / 2 < r).
-  { apply Z.div_lt_upper_bound; omega. }
-  simpl in *. omega.
+  { apply Z.div_lt_upper_bound; lia. }
+  lia.
 Qed.
 Next Obligation.
-  replace n with H; eauto.
+  replace n with H; auto with zarith.
   assert (n <= H) by eapply l0, H4, Z.le_refl.
   assert (H <= n) by eapply H3, e, Z.le_refl.
-  omega.
+  lia.
   Unshelve.
-  - eauto.
-  - destruct (Z_le_gt_dec ((l + r) / 2) H) as [ Hle | ]; simpl in *; try omega.
-    generalize H1. erewrite (e _ _ Hle). inversion 1.
+  - auto.
+  - destruct (Z_le_gt_dec ((l + r) / 2) H) as [ Hle | ]; auto with zarith.
+    generalize H1. erewrite (e _ _ Hle). congruence.
 Qed.
 Next Obligation.
   exists H. repeat split; eauto.
-  destruct (Z_le_gt_dec ((l + r) / 2) H) as [ Hle | ]; simpl in *; try omega.
-  generalize H1. erewrite (e _ _ Hle). inversion 1.
+  destruct (Z_le_gt_dec ((l + r) / 2) H) as [ Hle | ]; auto with zarith.
+  generalize H1. erewrite (e _ _ Hle). congruence.
 Qed.
 Next Obligation.
   assert (Z.succ l <= (l + r) / 2).
-  { apply Z.div_le_lower_bound; omega. }
+  { apply Z.div_le_lower_bound; lia. }
   assert ((l + r) / 2 <= r).
-  { apply Z.div_le_upper_bound; omega. }
-  unfold ltof. simpl in *. apply Z2Nat.inj_lt; omega.
+  { apply Z.div_le_upper_bound; lia. }
+  apply Z2Nat.inj_lt; simpl; lia.
 Qed.
 Next Obligation.
   assert (Z.succ l <= (l + r) / 2).
-  { apply Z.div_le_lower_bound; omega. }
-  unfold ltof. simpl in *. omega.
+  { apply Z.div_le_lower_bound; lia. }
+  lia.
 Qed.
 Next Obligation.
   replace n with H; eauto.
   assert (H <= n) by eapply H3, e, Z.le_refl.
   assert (n <= H) by eapply l0, H4, Z.le_refl.
-  omega.
+  lia.
   Unshelve.
   - eauto.
-  - generalize (l0 _ _ H1). omega.
+  - lia.
 Qed.
-Next Obligation.
-  exists H. repeat split; eauto.
-Qed.
+Next Obligation. eauto 8. Qed.
 
 Definition upper_bound l r p :=
   upper_bound' l r p (well_founded_ltof _ _ _).
@@ -114,66 +109,61 @@ Program Fixpoint lower_bound' l r
             let (n, _) := lower_bound' m r (fun n _ => p n _) (Hacc' _ _) _ in
             exist _ n _ ) eq_refl
   end.
-Next Obligation.
-  repeat split; intros; try omega.
-  eapply H3. omega.
-Qed.
+Next Obligation. auto with zarith. Qed.
 Next Obligation.
   split.
-  - assert (Z.succ l <= (l + r) / 2) by (apply (Z.div_le_lower_bound _ 2); omega). omega.
-  - apply (Z.div_le_upper_bound _ 2); omega.
+  - assert (Z.succ l <= (l + r) / 2) by (apply (Z.div_le_lower_bound _ 2); lia). lia.
+  - apply (Z.div_le_upper_bound _ 2); lia.
 Qed.
 Next Obligation.
   assert ((l + r) / 2 <= r).
-  { apply Z.div_le_upper_bound; omega. }
+  { apply Z.div_le_upper_bound; lia. }
   assert (Z.succ l <= (l + r) / 2).
-  { apply Z.div_le_lower_bound; omega. }
-  unfold ltof. simpl in *. apply Z2Nat.inj_lt; omega.
+  { apply Z.div_le_lower_bound; lia. }
+  apply Z2Nat.inj_lt; simpl; lia.
 Qed.
 Next Obligation.
   assert (Z.succ l <= (l + r) / 2).
-  { apply Z.div_le_lower_bound; omega. }
-  simpl in *. omega.
+  { apply Z.div_le_lower_bound; lia. }
+  lia.
 Qed.
 Next Obligation.
   replace n with H; eauto.
   assert (n <= H) by eapply H3, e, Z.le_refl.
   assert (H <= n) by eapply l0, H4, Z.le_refl.
-  omega.
+  lia.
   Unshelve.
-  - destruct (Z_le_gt_dec H ((l + r) / 2)) as [ Hle | ]; simpl in *; try omega.
-    generalize H1. erewrite (e _ _ Hle). inversion 1.
+  - destruct (Z_le_gt_dec H ((l + r) / 2)) as [ Hle | ]; try lia.
+    generalize H1. erewrite (e _ _ Hle). congruence.
   - eauto.
 Qed.
 Next Obligation.
   exists H. repeat split; eauto.
-  destruct (Z_le_gt_dec H ((l + r) / 2)) as [ Hle | ]; simpl in *; try omega.
-  generalize H1. erewrite (e _ _ Hle). inversion 1.
+  destruct (Z_le_gt_dec H ((l + r) / 2)) as [ Hle | ]; try lia.
+  generalize H1. erewrite (e _ _ Hle). congruence.
 Qed.
 Next Obligation.
   assert (l <= (l + r) / 2).
-  { apply Z.div_le_lower_bound; omega. }
+  { apply Z.div_le_lower_bound; lia. }
   assert ((l + r) / 2 < r).
-  { apply Z.div_lt_upper_bound; omega. }
-  unfold ltof. simpl in *. apply Z2Nat.inj_lt; omega.
+  { apply Z.div_lt_upper_bound; lia. }
+  apply Z2Nat.inj_lt; simpl; omega.
 Qed.
 Next Obligation.
   assert ((l + r) / 2 < r).
-  { apply Z.div_lt_upper_bound; omega. }
-  unfold ltof. simpl in *. omega.
+  { apply Z.div_lt_upper_bound; lia. }
+  lia.
 Qed.
 Next Obligation.
-  replace n with H; eauto.
+  replace n with H; auto.
   assert (H <= n) by eapply l0, H4, Z.le_refl.
   assert (n <= H) by eapply H3, e, Z.le_refl.
-  omega.
+  lia.
   Unshelve.
-  - eauto.
-  - generalize (l0 _ _ H1). omega.
+  - auto.
+  - auto with zarith.
 Qed.
-Next Obligation.
-  exists H. repeat split; eauto.
-Qed.
+Next Obligation. eauto 8. Qed.
 
 Definition lower_bound l r p :=
   lower_bound' l r p (well_founded_ltof _ _ _).
@@ -183,8 +173,9 @@ Extract Inductive sumbool => "bool" ["true" "false"].
 Extract Inductive positive => int ["(fun n -> 2 * n + 1)" "(( * ) 2)" "1"] "(fun fI fO f1 n -> if n = 1 then f1 () else if n mod 2 = 0 then fO (n / 2) else fI (n / 2))".
 Extract Inductive Z => int ["0" "" "( ~- )"] "(fun fO fpos fneg n -> if n = 0 then fO () else if 0 < n then fpos n else fneg (~-n))".
 
-Extract Constant Z.add => "( + )".
-Extract Constant Z.sub => "( - )".
-Extract Constant Z.div => "( / )".
-Extract Constant Z_le_gt_dec => "( <= )".
+Extract Inlined Constant Z.succ => "succ".
+Extract Inlined Constant Z.add => "( + )".
+Extract Inlined Constant Z.sub => "( - )".
+Extract Inlined Constant Z.div => "( / )".
+Extract Inlined Constant Z_le_gt_dec => "( <= )".
 Extraction "bsearch.ml" lower_bound upper_bound.
