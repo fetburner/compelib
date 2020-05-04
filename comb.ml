e4325204a3f7abd66d2948dbd8edb083a97c434c
fetburner <masayuki2718@yahoo.co.jp>
2020-05-04 17:27:36 +0900

    Implement combinations with repetition

--- comb.ml
+++ comb.ml
@@ -1,14 +1,32 @@
 let rec comb acc xs n ys =
   match n, ys with
-  | 0, _ -> xs :: acc
+  | 0, _ -> List.rev xs :: acc
   | _, [] -> acc
   | n, y :: ys -> comb (comb acc xs n ys) (y :: xs) (n - 1) ys
 (* 与えられたリストからn要素を選ぶ組み合わせを列挙 *)
 let comb : int -> 'a list -> 'a list list = fun n xs -> comb [] [] n xs
 
+let rec repcomb acc xs n ys =
+  match n, ys with
+  | 0, _ -> List.rev xs :: acc
+  | _, [] -> acc
+  | n, y :: ys' -> repcomb (repcomb acc xs n ys') (y :: xs) (n - 1) ys
+(* 与えられたリストから重複を許してn要素を選ぶ組み合わせを列挙 *)
+let repcomb : int -> 'a list -> 'a list list = fun n xs -> repcomb [] [] n xs
+
 (* sample code *)
 comb 2 [1; 2; 3];;
+repcomb 2 [1; 2; 3];;
+
 comb 3 [1; 2; 3];;
+repcomb 3 [1; 2; 3];;
+
 comb 0 [1; 2; 3];;
+repcomb 0 [1; 2; 3];;
+
 comb 4 [1; 2; 3];;
+repcomb 4 [1; 2; 3];;
+
+(* 添字の違う要素は区別される *)
 comb 2 [1; 1; 2];;
+repcomb 2 [1; 1; 2];;
