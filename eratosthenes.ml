7a140eddcbaf0ae40cfc2057ee35385bf0f05b26
fetburner <masayuki2718@yahoo.co.jp>
2020-05-12 01:06:30 +0900

    Improve the sieve of Eratosthenes

--- eratosthenes.ml
+++ eratosthenes.ml
@@ -5,39 +5,47 @@ let sieve :
   (* 素数ならtrueを返す関数 *)
   (int -> bool)
   = fun n ->
+    (* 今ふるおうとしている整数 *)
+    let p = ref 3 in
+    (* pがどのインデックスに対応するか *)
+    let i = ref 0 in
+    (* pの2乗 *)
+    let pp = ref 9 in
+    (* p^2がどのインデックスに対応するか *)
+    let ii = ref 3 in
     (*
      * 2の倍数は無視し，3, 5, 7, 9 ... が格納されているとみなす
      * よって添字から格納されている数へ変換する式は 2i + 3
      * 格納されている数から添字へ変換する式は (x - 3) / 2
      *)
-    let m = n / 2 - 1 in
+    let m = (n lsr 1) - 1 in
     let a = Array.make m true in
-    for i = 0 to (int_of_float (sqrt @@ float_of_int n) - 3) / 2 do
-      if a.(i) then begin
-        let p = 2 * i + 3 in
-        let rec loop i =
-          if i < m then begin
-            a.(i) <- false;
-            loop (i + p)
-          end in
-        loop ((p * p - 3) / 2)
-      end
-    done;
-    function 
-      | 0 | 1 -> false
+    let rec sieve_aux k =
+      if k < !pp
+      then a.((k - 3) lsr 1)
+      else begin
+        let rec mark i =
+          if i < m then (a.(i) <- false; mark (i + !p)) in
+        (if a.(!i) then mark !ii);
+        incr i;
+        p := !p + 2;
+        pp := !pp + (!p lsl 1);
+        ii := !ii + (!p lsl 1) - 2;
+        sieve_aux k
+      end in
+    function
+      | k when k <= 1 -> false
       | 2 -> true
-      | n -> n mod 2 = 1 && a.((n - 3) / 2)
+      | k -> k land 1 = 1 && sieve_aux k
+
 
 (* sample code *)
 
-let p = sieve 100;;
-List.init 100 (fun i -> i)
-  |> List.filter p;;
-let p = sieve 3;;
-List.init 3 p;;
-let p = sieve 4;;
-List.init 4 p;;
-let p = sieve 5;;
-List.init 5 p;;
-let p = sieve 6;;
-List.init 6 p;;
+(* 呼び出しごとに途中結果がシェアされるので，こういうことしても平気 *)
+List.filter (sieve 1000000) (List.init 1000000 Fun.id);;
+
+(* 入力が小さい時の挙動も調べておきたい *)
+List.init 3 (sieve 3);;
+List.init 4 (sieve 4);;
+List.init 5 (sieve 5);;
+List.init 6 (sieve 6);;
