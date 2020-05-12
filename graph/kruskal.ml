84e62c5e26a9071fa13d1763d971a44c7641a4c1
fetburner <masayuki2718@yahoo.co.jp>
2020-05-13 03:06:03 +0900

    Fix the implementation of Kruskal's algorithm

--- graph/kruskal.ml
+++ graph/kruskal.ml
@@ -1,13 +1,11 @@
 module WeightedGraph
-  (Vertex : sig
-    type t
-    val compare : t -> t -> int
-  end)
   (Weight : sig
     type t
     val compare : t -> t -> int
   end) :
 sig
+  type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
+
   (*
    * HashtblやMapを用いているとプリム法と大差ない計算時間になってしまうため，
    * より汎用的な実装が欲しければそちらを当たること
@@ -16,35 +14,43 @@ sig
     (* 頂点の数n *)
     int ->
     (* 辺のリスト *)
-    (* 頂点は0からn-1までの整数でなくてはならない *)
+    (* 頂点は0からn-1までの整数でなくてはならない
+       ソートにList.sortを使っているせいでデータ構造が固定されており，あまり良くない… *)
     (int * int * Weight.t) list ->
-    (* 最小全域木に含まれる辺のリスト *)
-    (int * int * Weight.t) list
+    (* 最小全域木に含まれる辺のリスト
+       畳み込む関数と初期値まで与えられて初めて計算を開始するので，
+       最小全域木に対して色々な操作をしたい場合は，
+       一度リストにして覚えておくとよい．
+       最小全域木の重さの和だけ欲しいとか一回しか使わない場合は，
+       直接畳み込む関数と初期値を与えるとdeforestationになっていい感じ． *)
+    (int * int * Weight.t) church_list
 end =
 struct
+  type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
+
   let kruskal n es =
-    let uf = RawUnionFind.make n in
-    List.fold_left (fun acc (u, v, w) ->
-      if RawUnionFind.compare_class (RawUnionFind.find uf u) (RawUnionFind.find uf v) = 0
-      then acc
-      else begin
-        RawUnionFind.unite uf u v;
-        (u, v, w) :: acc
-      end) [] (List.sort (fun (_, _, w) (_, _, w') -> Weight.compare w w') es)
+    { fold = fun f init ->
+      let uf = UnionFind.make n in
+      List.fold_left (fun acc (u, v, w) ->
+        let c = UnionFind.find uf u in
+        let d = UnionFind.find uf v in
+        if UnionFind.Class.compare c d = 0
+        then acc
+        else (UnionFind.unite uf c d; f (u, v, w) acc)) init @@
+      List.sort (fun (_, _, w) (_, _, w') -> Weight.compare w w') es }
 end
 
 (* sample code *)
 
-module G = WeightedGraph (String) (struct
+module G = WeightedGraph (struct
   type t = int
   let compare = compare
 end);;
 
-G.kruskal 7
+(G.kruskal 7
   [(0, 1, 7); (0, 3, 5);
    (1, 0, 7); (1, 2, 8); (1, 3, 9); (1, 4, 7);
    (2, 1, 8); (2, 4, 5);
    (3, 0, 5); (3, 1, 9); (3, 4, 15); (3, 5, 6);
    (4, 1, 7); (4, 2, 5); (4, 3, 15); (4, 5, 8); (4, 6, 9);
-   (5, 3, 6); (5, 4, 8); (5, 6, 11);
-   (6, 4, 9); (6, 5, 11)];;
+   (5, 3, 6); (5, 4, 8); (5, 6, 11); (6, 4, 9); (6, 5, 11)]).fold List.cons [];;
