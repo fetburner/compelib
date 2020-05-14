3207e474e43ff4942ce42f97c722ec569f776b28
fetburner <masayuki2718@yahoo.co.jp>
2020-05-14 17:27:23 +0900

    Implement a special form of Dijkstra's algorithm

--- graph/dijkstra.ml
+++ graph/dijkstra.ml
@@ -220,6 +220,73 @@ end
   end
 end
 
+module IntWeightedDirectedGraphByArray = struct
+  include (WeightedDirectedGraph.ByArray.Make (struct
+    type t = int
+    let zero = 0
+    let inf = max_int
+    let ( + ) = ( + )
+    let compare = compare
+  end))
+
+  (* 辺の重さがごく小さい整数の場合は，Mapをキュー代わりに使うより配列を使った方が効率的
+     時間計算量はO(E+VW)，空間計算量はO(E+W) *)
+  let dijkstra_special :
+    (* 頂点数n *)
+    int ->
+    (* 辺の重さの上限W *)
+    int ->
+    (* 頂点uを受け取って，
+       uから伸びる辺の終点vと重さwの組みのリストを返す関数
+       辺の重さwはW以下でなくてはならない *)
+    (int -> (int * int) church_list) ->
+    (* 始点 *)
+    int ->
+    (* 始点から辿り着けなければinfを返す関数
+       この関数を覚えておけば，呼び出しごとの途中までの計算結果がシェアされる *)
+    (int -> int)
+  = fun n mw es s ->
+    (* 各頂点への最短距離を格納するやつ *)
+    let d = Array.make n max_int in
+    (* キュー *)
+    let q = Array.make (mw + 1) [] in
+    (* ヒープの最小要素の重み *)
+    let w = ref 0 in
+    (* ヒープの最小要素がどのインデックスに対応するか *)
+    let i = ref 0 in
+    (* ヒープに格納されている要素の数 *)
+    let m = ref 1 in
+    d.(s) <- 0;
+    q.(0) <- [s];
+    let rec dijkstra_aux t =
+      (* 既に終点までの距離が分かっているので返す *)
+      if !m <= 0 || d.(t) <= !w then d.(t) else
+      match q.(!i) with
+      (* ヒープには重さがwの要素は無かった *)
+      | [] ->
+          incr w;
+          incr i;
+          if mw < !i then i := 0;
+          dijkstra_aux t
+      (* ヒープには重さwの要素uが存在する *)
+      | u :: us ->
+          (* ヒープから重さwの要素を削除する *)
+          decr m;
+          q.(!i) <- us;
+          if !w <= d.(u) then
+            (* 未だ頂点uを訪れていない *)
+            (es u).fold (fun (v, c) () ->
+              if !w + c < d.(v) then begin
+                d.(v) <- !w + c; (* 頂点vの重さを緩和 *)
+                (* ヒープにvを追加する *)
+                let j = !i + c - if !i + c <= mw then 0 else mw + 1 in
+                q.(j) <- v :: q.(j);
+                incr m
+              end) ();
+          dijkstra_aux t in
+    dijkstra_aux
+end
+
 (* sample code *)
 
 module Float = struct
@@ -230,25 +297,25 @@ module Float = struct
   let compare = compare
 end
 
-module G = WeightedDirectedGraph.ByArray.Make (Float)
+module G = IntWeightedDirectedGraphByArray
 
 let e =
-  [|[ (1, 7.); (2, 9.); (5, 14.) ];
-    [ (0, 7.); (2, 10.); (3, 15.) ];
-    [ (0, 9.); (1, 10.); (3, 11.); (5, 2.) ];
-    [ (1, 15.); (2, 11.); (4, 6.) ];
-    [ (3, 6.); (5, 9.) ];
-    [ (0, 14.); (2, 2.); (4, 9.) ]|];;
+  [|[ (1, 7); (2, 9); (5, 14) ];
+    [ (0, 7); (2, 10); (3, 15) ];
+    [ (0, 9); (1, 10); (3, 11); (5, 2) ];
+    [ (1, 15); (2, 11); (4, 6) ];
+    [ (3, 6); (5, 9) ];
+    [ (0, 14); (2, 2); (4, 9) ]|];;
 
-List.init 7 @@ Fun.flip (G.dijkstra 7) 0 @@ fun u ->
+List.init 6 @@ Fun.flip (G.dijkstra_special 6 16) 0 @@ fun u ->
   { G.fold = fun f -> List.fold_right f e.(u) };;
 
 module G' = WeightedDirectedGraph.ByArray.Make
   (struct
-    type t = float * (string list -> string list)
-    let inf = (infinity, fun xs -> xs)
-    let zero = (0., fun xs -> xs)
-    let ( + ) (c, f) (d, g) = (c +. d, fun xs -> f (g xs))
+    type t = int * (string list -> string list)
+    let inf = (max_int, fun xs -> xs)
+    let zero = (0, fun xs -> xs)
+    let ( + ) (c, f) (d, g) = (c + d, fun xs -> f (g xs))
     let compare (c, _) (d, _) = compare c d
   end)
 
@@ -266,14 +333,20 @@ List.init 6 @@ Fun.flip (G'.dijkstra 7) 0 @@ fun u ->
 module IntPair = struct
   type t = int * int
   let compare = compare
-end
+  end
 
-module G = WeightedDirectedGraph.ByMap.Make (IntPair) (Float)
+module G = WeightedDirectedGraph.ByMap.Make (IntPair) (struct
+  type t = int
+  let inf = max_int 
+  let zero = 0
+  let ( + ) = ( + )
+  let compare = compare
+end)
 
 let d = Fun.flip G.dijkstra (0, 0) @@ fun (x, y) ->
   { G.fold = fun f ->
     List.fold_right f @@
-    List.map (fun v -> (v, float_of_int (abs x + abs y)))
+    List.map (fun v -> (v, abs x + abs y))
     [(x + 1, y); (x - 1, y); (x, y + 1); (x, y - 1)] };;
 List.init 10 @@ fun x ->
   List.init 10 @@ fun y -> d (x, y);;
