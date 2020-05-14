69cce3a10770249084de1ecdfc649114cc7823bf
fetburner <masayuki2718@yahoo.co.jp>
2020-05-14 13:38:31 +0900

    Improve the implementation of Dinic's algorithm

--- graph/bfs.ml
+++ graph/bfs.ml
@@ -1,8 +1,100 @@
 module DirectedGraph
-  (* 頂点をキーとしたハッシュテーブルの実装 *)
-  (VHash : Hashtbl.S)
-  (* 道の表現 *)
-  (Path : sig
+: sig
+  (* 配列を用いたBFSの実装 *)
+  module ByArray : sig
+    module Make :
+      functor
+        (* 道の表現 *)
+        (Path : sig
+          type t
+          (* 辺の名前 *)
+          type edge
+          (* 長さ0の道 *)
+          val nil : t
+          (* 道の後ろに辺を付け足した道 *)
+          val snoc : t -> edge -> t
+        end) ->
+        sig
+          type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
+
+          (* BFSにより，重みのないグラフの最短経路を求める *)
+          val bfs :
+            (* 頂点数n *)
+            int ->
+            (* 辺の名前が付いた隣接リスト *)
+            (int -> (int * Path.edge) church_list) ->
+            (* 始点 *)
+            int ->
+            (* 始点からの最短経路を返す関数
+               この関数を覚えておけば，呼び出しごとの途中までの計算結果がシェアされる *)
+            (int -> Path.t option)
+        end
+  end
+
+  (* ハッシュテーブルを用いたBFSの実装 *)
+  module ByHashtbl : sig
+    module Make :
+      functor
+        (* 頂点をキーとしたハッシュテーブルの実装 *)
+        (VHash : Hashtbl.S)
+        (* 道の表現 *)
+        (Path : sig
+          type t
+          (* 辺の名前 *)
+          type edge
+          (* 長さ0の道 *)
+          val nil : t
+          (* 道の後ろに辺を付け足した道 *)
+          val snoc : t -> edge -> t
+        end) ->
+        sig
+          type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
+
+          (* BFSにより，重みのないグラフの最短経路を求める *)
+          val bfs :
+            (* 頂点数（ハッシュテーブルを用いるので目安程度） *)
+            int ->
+            (* 辺の名前が付いた隣接リスト *)
+            (VHash.key -> (VHash.key * Path.edge) church_list) ->
+            (* 始点 *)
+            VHash.key ->
+            (* 最短経路を返す関数 辿り着けなければNoneを返す） *)
+            (VHash.key -> Path.t option)
+        end
+  end
+
+  (* Mapを用いたBFSの実装 *)
+  module ByMap : sig
+    module Make :
+      functor
+        (* 頂点をキーとしたMapの実装 *)
+        (VMap : Map.S)
+        (* 道の表現 *)
+        (Path : sig
+          type t
+          (* 辺の名前 *)
+          type edge
+          (* 長さ0の道 *)
+          val nil : t
+          (* 道の後ろに辺を付け足した道 *)
+          val snoc : t -> edge -> t
+        end) ->
+        sig
+          type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
+
+          (* BFSにより，重みのないグラフの最短経路を求める *)
+          val bfs :
+            (* 辺の名前が付いた隣接リスト *)
+            (VMap.key -> (VMap.key * Path.edge) church_list) ->
+            (* 始点 *)
+            VMap.key ->
+            (* 最短経路を返す関数 辿り着けなければNoneを返す） *)
+            (VMap.key -> Path.t option)
+        end
+  end
+end
+= struct
+  module type Path = sig
     type t
     (* 辺の名前 *)
     type edge
@@ -10,48 +102,84 @@ module DirectedGraph
     val nil : t
     (* 道の後ろに辺を付け足した道 *)
     val snoc : t -> edge -> t
-  end) :
-sig
-  type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
+  end
 
-  (* BFSにより，重みのないグラフの最短経路を求める *)
-  val bfs :
-    (* 頂点数（ハッシュテーブルを用いるので目安程度） *)
-    int ->
-    (* 辺の名前が付いた隣接リスト *)
-    (VHash.key -> (VHash.key * Path.edge) church_list) ->
-    (* 始点 *)
-    VHash.key ->
-    (* 最短経路を返す関数 辿り着けなければNoneを返す） *)
-    (VHash.key -> Path.t option)
-end = struct
-  type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
+  (* 最短経路を格納するデータ構造を抽象化したBFSの実装 *)
+  module Core
+    (P : Path)
+    (* グラフの頂点を添字としたハッシュ *)
+    (VHash : sig
+      type t
+      type vertex (* グラフの頂点 *)
+      val find_opt : t -> vertex -> P.t option
+      val update : t -> vertex -> P.t -> unit
+    end)
+  = struct
+    type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
 
-  let rec bfs_aux es d frontier t =
-    match VHash.find_opt d t, !frontier with
-    (* もう既に全ての頂点までの経路が分かっている *)
-    | None, [] -> None
-    (* 既に終点までの経路が分かっているので返す *)
-    | Some _ as ans, _ -> ans
-    (* 終点までの経路が分かっていないので，BFSを続行 *)
-    | None, _ :: _ ->
-        frontier := List.fold_right (fun u ->
-          (es u).fold @@ fun (v, e) frontier ->
-            if VHash.mem d v
-            then frontier
-            else (VHash.add d v (Path.snoc (VHash.find d u) e); v :: frontier)) !frontier [];
-        bfs_aux es d frontier t
+    let bfs d es s =
+      let q = Queue.create () in
+      Queue.push s q;
+      VHash.update d s P.nil;
+      let rec bfs_aux t =
+        match VHash.find_opt d t with
+        (* 既に終点までの経路が分かっているので返す *)
+        | Some _ as ans -> ans
+        | None ->
+            match Queue.take q with
+            (* もう既に全ての頂点までの経路が分かっている *)
+            | exception Queue.Empty -> None
+            (* 終点までの経路が分かっていないので，BFSを続行 *)
+            | u ->
+                let Some p = VHash.find_opt d u in
+                (es u).fold (fun (v, e) () ->
+                  if Option.is_none (VHash.find_opt d v) then
+                    (Queue.push v q; VHash.update d v (P.snoc p e))) ();
+                bfs_aux t in
+      bfs_aux
+  end
 
-  (*
-   * 終点に辿り着いた時点で探索を切り上げるが，
-   * 戻り値の関数を覚えておくと，途中まで探索した結果が再利用される
-   * （#trace bfs_aux すると分かりやすい）
-   *)
-  let bfs n es s =
-    let d = VHash.create n in
-    VHash.add d s Path.nil;
-    let frontier = ref [s] in
-    bfs_aux es d frontier
+  module ByArray = struct
+    module Make (P : Path) = struct
+      module C = Core (P) (struct
+        type t = P.t option array
+        type vertex = int
+        let find_opt = Array.get
+        let update d v p = d.(v) <- Some p
+      end)
+      include C
+
+      let bfs n e s = C.bfs (Array.make n None) e s
+    end
+  end
+
+  module ByHashtbl = struct
+    module Make (VHash : Hashtbl.S) (P : Path) = struct
+      module C = Core (P) (struct
+        type t = P.t VHash.t
+        type vertex = VHash.key
+        let update = VHash.add
+        let find_opt = VHash.find_opt
+      end)
+      include C
+
+      let bfs n e s = C.bfs (VHash.create n) e s
+    end
+  end
+
+  module ByMap = struct
+    module Make (VMap : Map.S) (P : Path) = struct
+      module C = Core (P) (struct
+        type t = P.t VMap.t ref
+        type vertex = VMap.key
+        let find_opt d v = VMap.find_opt v !d
+        let update d v p = d := VMap.add v p !d
+      end)
+      include C
+
+      let bfs e s = C.bfs (ref VMap.empty) e s
+    end
+  end
 end
 
 module IntPairHash = Hashtbl.Make
@@ -62,7 +190,7 @@ module IntPairHash = Hashtbl.Make
   end)
 
 (* 経路情報も欲しい場合 *)
-module G = DirectedGraph
+module G = DirectedGraph.ByHashtbl.Make
 (IntPairHash)
 (struct
   type t = string list
@@ -90,7 +218,7 @@ d (5, 0);;
 d (3, 2);;
 
 (* 距離だけ欲しい場合 *)
-module G = DirectedGraph (IntPairHash)
+module G = DirectedGraph.ByHashtbl.Make (IntPairHash)
 (struct
   type t = int
   type edge = unit
--- graph/dinic.ml
+++ graph/dinic.ml
@@ -1,6 +1,4 @@
 module FlowNetwork
-  (* 頂点 *)
-  (Vertex : Hashtbl.HashedType)
   (* 流量 *)
   (Flow : sig
     type t
@@ -14,22 +12,22 @@ sig
   type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
 
   val max_flow :
-    (* 頂点の数（ハッシュテーブルを用いるので目安程度） *)
+    (* 頂点の数n *)
     int ->
-    (* 各辺とその容量 *)
-    (Vertex.t * Vertex.t * Flow.t) church_list ->
+    (* 各辺と逆辺の容量のリスト
+       頂点は0からn-1までの整数でなくてはならない *)
+    (int * int * Flow.t * Flow.t) church_list ->
     (* 始点 *)
-    Vertex.t ->
+    int ->
     (* 終点 *)
-    Vertex.t ->
+    int ->
     (* 最大フロー *)
     Flow.t *
     (* 最大フローを流した際の各辺の流量 *)
-    (Vertex.t * Vertex.t * Flow.t) church_list
+    (int * int * Flow.t) church_list
 end =
 struct
-  module VHash = Hashtbl.Make (Vertex)
-  module G = DirectedGraph (VHash) (struct
+  module G = DirectedGraph.ByArray.Make (struct
     type t = int
     type edge = unit
     let nil = 0
@@ -37,78 +35,73 @@ struct
   end)
 
   type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
-  type edge = { to_ : Vertex.t; mutable capacity : Flow.t; is_rev : bool; rev : edge }
+  type edge = { from : int; to_ : int; mutable capacity : Flow.t; original_capacity : Flow.t; rev : edge }
 
   let max_flow n es s t =
-    assert (not (Vertex.equal s t));
+    assert (s <> t);
     (* 逆辺を張りつつ，隣接リスト形式に変換 *)
-    let adj = VHash.create n in
-    es.fold (fun (u, v, c) () ->
-      let rec edge = { to_ = v; capacity = c; is_rev = false; rev = rev_edge }
-      and rev_edge = { to_ = u; capacity = Flow.zero; is_rev = true; rev = edge } in
-      VHash.add adj u edge;
-      VHash.add adj v rev_edge) ();
+    let adj = Array.make n [] in
+    es.fold (fun (u, v, c, c') () ->
+      let rec edge = { from = u; to_ = v; capacity = c; original_capacity = c; rev = rev_edge }
+      and rev_edge = { from = v; to_ = u; capacity = c'; original_capacity = c'; rev = edge } in
+      adj.(u) <- edge :: adj.(u);
+      adj.(v) <- rev_edge :: adj.(v)) ();
 
+    let iter = Array.make n [] in
     (* 増加パスをDFSで探し，流せるだけ流していく *)
-    let rec dfs iter level v f =
-      if Vertex.equal v t
+    let rec dfs level v f =
+      if v = t
       then f
       else begin
         let rec find () =
-          match VHash.find iter v with
-          | exception Not_found -> Flow.zero
-          | e ->
-              VHash.remove iter v;
+          match iter.(v) with
+          | [] -> Flow.zero
+          | e :: rest ->
+              iter.(v) <- rest;
               let d =
                 if Flow.compare e.capacity Flow.zero <= 0 || level e.to_ <= level v
                 then Flow.zero
-                else dfs iter level e.to_ @@
-                  if Flow.compare f e.capacity <= 0 then f else e.capacity in
+                else dfs level e.to_ @@
+                       if Flow.compare f e.capacity <= 0 then f else e.capacity in
               if Flow.compare d Flow.zero <= 0
               then find ()
               else begin
                 let open Flow in
                 e.capacity <- e.capacity - d;
                 e.rev.capacity <- e.rev.capacity + d; d
-              end in 
+              end in
         find ()
       end in
 
     let rec outer flow =
       let level = G.bfs n (fun v ->
-        { G.fold = fun f ->
-          List.fold_right (fun e acc ->
+        { G.fold = fun f acc ->
+          List.fold_left (fun acc e ->
             if Flow.compare e.capacity Flow.zero <= 0
             then acc
-            else f (e.to_, ()) acc) @@
-          VHash.find_all adj v }) s in
+            else f (e.to_, ()) acc) acc adj.(v) }) s in
       match level t with
       | None -> flow
       | Some _ ->
-          let iter = VHash.copy adj in
           let rec inner flow =
-            let f = dfs iter level s Flow.inf in
+            let f = dfs level s Flow.inf in
             if Flow.compare f Flow.zero <= 0
             then flow
             else let open Flow in inner (flow + f) in
+          Array.iteri (Array.set iter) adj;
           outer (inner flow) in
 
-    let f = outer Flow.zero in
-    let open Flow in
-    (f, { fold = fun f ->
-      VHash.fold (fun u e acc ->
-        if e.is_rev
-        then acc
-        else f (u, e.to_,  e.rev.capacity) acc) adj })
+    (outer Flow.zero, { fold = fun f ->
+      Array.fold_right
+        (Fun.flip (List.fold_left (fun acc e ->
+          let open Flow in
+          if 0 <= Flow.compare e.capacity e.original_capacity
+          then acc
+          else f (e.from, e.to_, e.original_capacity - e.capacity) acc))) adj })
 end;;
 
 (* 蟻本p. 188のグラフで試す *)
 module G = FlowNetwork
-(struct
-  type t = int
-  let equal = ( = )
-  let hash = Hashtbl.hash
-end)
 (struct
   type t = int
   let inf = max_int
@@ -120,8 +113,8 @@ end);;
 
 let (f, e) = G.max_flow 5
   { fold = fun f -> List.fold_right f 
-    [ (0, 1, 10); (0, 2, 2);
-      (1, 2, 6); (1, 3, 6);
-      (2, 4, 5);
-      (3, 2, 3); (3, 4, 8) ] } 0 4;;
+    [ (0, 1, 10, 0); (0, 2, 2, 0);
+      (1, 2, 6, 0); (1, 3, 6, 0);
+      (2, 4, 5, 0);
+      (3, 2, 3, 0); (3, 4, 8, 0) ] } 0 4;;
 e.fold List.cons [];;
