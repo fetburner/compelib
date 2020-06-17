ca73fb17bc807d7ca9d8dd29cefec26ad5e77743
fetburner <masayuki2718@yahoo.co.jp>
2020-06-17 16:37:15 +0900

    Modify the implementation of 01-BFS

--- graph/bfs01.ml
+++ graph/bfs01.ml
@@ -1,50 +1,58 @@
 module Weighted01DirectedGraph
-  (* 道の表現 *)
-  (Path : sig
+  (* 頂点を添字，経路長を要素とした配列の実装 *)
+  (Array : sig
     type t
-    (* 辺の名前 *)
-    type edge
-    (* 長さ0の道 *)
-    val nil : t
-    (* 道の後ろに辺を付け足した道 *)
-    val snoc : t -> edge -> t
-  end) :
-sig
-  val bfs01 :
-    (* 頂点数 *)
-    int ->
-    (* 辺の名前が付いた隣接リスト *)
-    (* 辺の重さが0ならfalse，1ならtrue *)
-    ('v -> ('v * Path.edge * bool) list) ->
+    type key
+    type elt = int (* 重みのない（=全ての重さが0か1な）グラフなので経路長は非負整数 *)
+    val get : t -> key -> elt
+    val set : t -> key -> elt -> unit
+  end)
+: sig
+  type vertex = Array.key
+
+  val shortest_path :
+    (* 全ての頂点についての経路長が無限大で初期化された配列 *)
+    Array.t ->
+    (* 最短経路を求めたいグラフの，ある頂点から伸びる辺に対してのイテレータ
+       重みが0の辺に対してはf0を，1の辺に対してはf1を用いる *)
+    (vertex -> f0:(vertex -> unit) -> f1:(vertex -> unit) -> unit) ->
     (* 始点 *)
-    'v ->
-    (* 最短経路を返す関数 辿り着けなければNoneを返す） *)
-    ('v -> Path.t option)
+    vertex ->
+    (* 終点を受け取って，始点からの最短距離を返す関数
+       始点から辿り着けない場合，無限大を返す
+       この関数を覚えておけば，呼び出しごとの途中までの計算結果がシェアされる *)
+    (vertex -> int)
 end =
 struct
-  let bfs01 n es s =
-    (* 始点sからの経路 *)
-    let d = Hashtbl.create n in
-    (* 現在BFSで走査している頂点のリスト *)
-    let vps = ref [(s, Path.nil)] in
+  type vertex = Array.key
+
+  let shortest_path d es s =
+    (* 始点への経路長を0にする *)
+    Array.set d s 0;
+    let w = ref 1 in
+    let q = ref [s] in
     let rec bfs t =
-      match Hashtbl.find_opt d t, !vps with
-      (* もう既に全ての頂点までの経路が分かっている *)
-      | None, [] -> None
-      (* 既に終点までの経路が分かっているので返す *)
-      | Some _ as ans, _ -> ans
-      (* 終点までの経路が分かっていないので，01BFSを続行 *)
-      | None, _ :: _ -> vps := List.fold_left dfs [] !vps; bfs t
-    (* 重さ0の辺をDFSで縮約 *)
-    and dfs vps (v, p) =
-      if Hashtbl.mem d v then vps
-      else begin
-        Hashtbl.add d v p;
-        List.fold_left (fun vps (u, e, b) ->
-          if b
-          then (u, Path.snoc p e) :: vps
-          else dfs vps (u, Path.snoc p e)) vps (es v)
-      end
-    in bfs
+      match !q with
+      (* もう既に全ての頂点までの経路が分かっているので返す *)
+      | [] -> Array.get d t
+      | us ->
+          match Array.get d t with
+          (* 既に終点までの距離が分かっているので返す *)
+          | x when x < !w -> x
+          (* 終点までの距離が分かっていないので，BFSを続行 *)
+          | _ ->
+              q := [];
+              List.iter dfs us;
+              incr w;
+              bfs t
+    and dfs u =
+      es u
+        ~f0:(fun v ->
+          if !w <= Array.get d v then
+            (Array.set d v (!w - 1); dfs v))
+        ~f1:(fun v ->
+          if !w < Array.get d v then
+            (Array.set d v !w; q := v :: !q)) in
+    bfs
 end
 
