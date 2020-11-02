44b27bf692853117ead7692a00ad2d672a15d540
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-03 02:41:36 +0900

    Manage the test for cycle detection

--- lib/cycleDetection.ml
+++ lib/cycleDetection.ml
@@ -29,17 +29,4 @@ let detect_cycle :
     else tortoise_and_tortoise (1 + acc) (f u) (f v) k in
   tortoise_and_hare 0 s s @@ fun _ u ->
     tortoise_and_tortoise 0 s u @@ fun m v ->
-      tortoise_and_hare 0 v v @@ fun l _ -> (m, l);;
-
-(* test *)
-detect_cycle ( = ) (function
-  | 0 -> 1
-  | 1 -> 2
-  | 2 -> 3
-  | 3 -> 4
-  | 4 -> 5
-  | 5 -> 6
-  | 6 -> 7
-  | 7 -> 2
-  | x -> x) 0
-
+      tortoise_and_hare 0 v v @@ fun l _ -> (m, l)
