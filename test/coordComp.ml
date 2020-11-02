85020114a7c4396d9d6ced9ddc9c255650528076
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-03 01:55:05 +0900

    Manage tests for coord compression

---
+++ test/coordComp.ml
@@ -0,0 +1,16 @@
+module CC = CoordComp.CoordComp (struct
+  type t = int
+  let compare = compare
+end)
+
+let l = [3; 1; 4; 1]
+let n, f, g = CC.compress l
+let%test "compress [3; 1; 4; 1] cardinal" = n = List.length (List.sort_uniq compare l)
+let%test "compress [3; 1; 4; 1] compression" = List.map f l = [1; 0; 2; 0]
+let%test "compress [3; 1; 4; 1] decompression" = List.init n g = List.sort_uniq compare l
+
+let l = [100; 100; 100]
+let n, f, g = CC.compress l
+let%test "compress [100; 100; 100] cardinal" = n = List.length (List.sort_uniq compare l)
+let%test "compress [100; 100; 100] compression" = List.map f l = [0; 0; 0]
+let%test "compress [100; 100; 100] decompression" = List.init n g = List.sort_uniq compare l
