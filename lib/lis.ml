97c682b2ec9db045a6cf403bcff05a77f77b6438
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-05 01:01:23 +0900

    Manage the test for longest increasing subsequence

--- lib/lis.ml
+++ lib/lis.ml
@@ -1,4 +1,4 @@
-module LIS (ESet : Set.S) = struct
+module Lis (ESet : Set.S) = struct
   let lis as_ = ESet.cardinal @@ List.fold_left (fun l a ->
     match ESet.split a l with
     | _, true, _ -> l
@@ -6,8 +6,3 @@ module LIS (ESet : Set.S) = struct
         if ESet.is_empty lr then ESet.add a l
         else ESet.add a (ESet.remove (ESet.min_elt lr) l)) ESet.empty as_
 end
-
-(* example *)
-module IntLIS = LIS (Set.Make (struct type t = int let compare = compare end));;
-
-IntLIS.lis [4; 1; 6; 2; 8; 5; 7; 3]
