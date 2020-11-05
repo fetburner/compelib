51d7adfe3a4612c14ed1f322ed524ab29eff4ad6
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-05 23:48:44 +0900

    Manage the test for exponentiation

--- lib/power.ml
+++ lib/power.ml
@@ -2,26 +2,3 @@
 let rec power ( * ) e m n =
   if n <= 0 then e
   else power ( * ) (if n land 1 = 0 then e else m * e) (m * m) (n lsr 1);;
-
-(* test *)
-(let n = 1000000000 in
-power ( *. ) 1. (1. +. 1. /. float_of_int n) n)
-
-let fib n =
-  let
-    ((a, _),
-     (_, _)) =
-    power
-      (fun
-        ((a, b),
-         (c, d))
-        ((a', b'),
-         (c', d')) ->
-           ((a * a' + b * c', a * b' + b * d'),
-            (c * a' + d * c', c * b' + d * d')))
-      ((1, 0),
-       (0, 1))
-      ((1, 1),
-       (1, 0)) n in
-  a;;
-Array.init 10 fib;;
---
+++ test/power.ml
@@ -0,0 +1,12 @@
+let%test _ =
+  let n = 1000000000 in
+  Float.abs (Compelib.Power.power ( *. ) 1. (1. +. 1. /. float_of_int n) n -. 2.71828) <= 1e-5
+
+let fib n =
+  fst @@
+  Compelib.Power.power
+    (fun (fn1, fn_1) (fm1, fm_1) ->
+      let fnfm = (fn1 - fn_1) * (fm1 - fm_1) in
+      (fn1 * fm1 + fnfm, fnfm + fn_1 * fm_1)) (1, 1) (1, 0) n
+
+let%test _ = List.init 10 fib = [1; 1; 2; 3; 5; 8; 13; 21; 34; 55]
