1224b0e32e44118f17298b0a468adf2850373918
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-06 01:37:36 +0900

    Manage the test for Warshall-Floyd algorithm

--- lib/graph/warshallFloyd.ml
+++ lib/graph/warshallFloyd.ml
@@ -89,25 +89,3 @@ struct
     raw_warshall_floyd n d;
     fun u v -> try d.(u).(v) with _ -> Weight.inf
 end
-
-(* sample code *)
-
-module Int = struct
-  type t = int
-  let zero = 0
-  let ( + ) = ( + )
-  let inf = max_int
-  let neg_inf = min_int
-  let compare = compare
-end
-
-module G = WeightedDirectedGraph (Int)
-
-let d = G.warshall_floyd 5
-  { G.fold = fun f -> List.fold_right f
-  [ (0, 1, 4); (0, 4, 3);
-    (1, 0, 4); (1, 2, 2);
-    (2, 1, 2); (2, 3, 3); (2, 4, 2);
-    (3, 2, 3); (3, 4, 7);
-    (4, 0, 3); (4, 2, 2); (4, 3, 7) ] };;
-List.init 5 @@ fun i -> List.init 5 @@ d i;;
