2dbe2edee8823ccddb2d50ce92116463cd3e9dbe
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-06 03:17:24 +0900

    Refactor the namespace of Bellman-Ford algorithm

--- lib/graph/bellmanFord.ml
+++ lib/graph/bellmanFord.ml
@@ -1,4 +1,4 @@
-module WeightedDirectedGraph
+module F
   (Weight : sig
     type t
     val inf : t (* オーバーフローの恐れはないので，max_intとか突っ込んでも良い *)
@@ -6,24 +6,8 @@ module WeightedDirectedGraph
     val neg_inf : t (* オーバーフローの恐れはないので，min_intとか突っ込んでも良い *)
     val ( + ) : t -> t -> t
     val compare : t -> t -> int
-  end) :
-sig
-  type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
-
-  val bellman_ford :
-    (* 頂点数n *)
-    int ->
-    (* 辺のリスト
-       頂点は0からn-1までの整数でなくてはならない *)
-    (int * int * Weight.t) church_list ->
-    (* 始点 *)
-    int ->
-    (* 頂点を受け取り，そこまでの距離を返す関数
-       頂点に到達するパスが無ければinfを返し，
-       そこに到達するまでに負閉路があればneg_infを返す *)
-    (int -> Weight.t)
-end =
-struct
+  end)
+= struct
   type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
 
   let bellman_ford n es s =
