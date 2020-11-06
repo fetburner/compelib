ba95a6730104535c0c61ffa373e12dbcfde86c9d
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-06 22:19:55 +0900

    Add a .mli file for number theoretic algorithms

--- lib/numberTheory.ml
+++ lib/numberTheory.ml
@@ -7,13 +7,7 @@ let lcm n m = n / gcd n m * m
 
 type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
 
-let factorize :
-  (* 素因数分解する整数 *)
-  int ->
-  (* 素因数と指数の組のリスト
-     素因数について降順に出てくる *)
-  (int * int) church_list
-= fun n ->
+let factorize n =
   (* nをpで割れるだけ割って，割れた数c'にcを加えたものとn/p^c'を返す *)
   let rec iter_div p c n k =
     if 0 < n mod p
@@ -43,42 +37,36 @@ let divisors : int -> int church_list = fun n ->
         | _  -> raise Not_found in
       divisors [] acc 1 }
 
-(* エラトステネスのふるい *)
-let sieve :
-  (* 素数かどうかを判定する自然数の上界 *)
-  int ->
-  (* 素数ならtrueを返す関数 *)
-  (int -> bool)
-  = fun n ->
-    (* 今ふるおうとしている整数 *)
-    let p = ref 3 in
-    (* pがどのインデックスに対応するか *)
-    let i = ref 0 in
-    (* pの2乗 *)
-    let pp = ref 9 in
-    (* p^2がどのインデックスに対応するか *)
-    let ii = ref 3 in
-    (*
-     * 2の倍数は無視し，3, 5, 7, 9 ... が格納されているとみなす
-     * よって添字から格納されている数へ変換する式は 2i + 3
-     * 格納されている数から添字へ変換する式は (x - 3) / 2
-     *)
-    let m = (n lsr 1) - 1 in
-    let a = Array.make m true in
-    let rec sieve_aux k =
-      if k < !pp
-      then a.((k - 3) lsr 1)
-      else begin
-        let rec mark i =
-          if i < m then (a.(i) <- false; mark (i + !p)) in
-        (if a.(!i) then mark !ii);
-        incr i;
-        p := !p + 2;
-        pp := !pp + (!p lsl 1);
-        ii := !ii + (!p lsl 1) - 2;
-        sieve_aux k
-      end in
-    function
-      | k when k <= 1 -> false
-      | 2 -> true
-      | k -> k land 1 = 1 && sieve_aux k
+let sieve n =
+  (* 今ふるおうとしている整数 *)
+  let p = ref 3 in
+  (* pがどのインデックスに対応するか *)
+  let i = ref 0 in
+  (* pの2乗 *)
+  let pp = ref 9 in
+  (* p^2がどのインデックスに対応するか *)
+  let ii = ref 3 in
+  (*
+   * 2の倍数は無視し，3, 5, 7, 9 ... が格納されているとみなす
+   * よって添字から格納されている数へ変換する式は 2i + 3
+   * 格納されている数から添字へ変換する式は (x - 3) / 2
+   *)
+  let m = (n lsr 1) - 1 in
+  let a = Array.make m true in
+  let rec sieve_aux k =
+    if k < !pp
+    then a.((k - 3) lsr 1)
+    else begin
+      let rec mark i =
+        if i < m then (a.(i) <- false; mark (i + !p)) in
+      (if a.(!i) then mark !ii);
+      incr i;
+      p := !p + 2;
+      pp := !pp + (!p lsl 1);
+      ii := !ii + (!p lsl 1) - 2;
+      sieve_aux k
+    end in
+  function
+    | k when k <= 1 -> false
+    | 2 -> true
+    | k -> k land 1 = 1 && sieve_aux k
