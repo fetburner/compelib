8e8bcb207f98653564674a816ebdc2cd1b956b00
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-07 21:14:08 +0900

    Fix

--- lib/containers/pArray.mli
+++ lib/containers/pArray.mli
@@ -6,7 +6,7 @@ val make : int -> 'a -> 'a t
 val of_list : 'a list -> 'a t
 val get : 'a t -> int -> 'a
 val set : 'a t -> int -> 'a -> 'a t
-(** [set a n x] returns a persistent array replacing element number [n] of [a] with [x].
+(* [set a n x] returns a persistent array replacing element number [n] of [a] with [x].
    [a] is not modified. *)
 val length : 'a t -> int
 val to_list : 'a t -> 'a list
