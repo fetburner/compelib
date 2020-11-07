0643fd883978b71cd7188c3a4389a0e694353077
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-07 19:48:34 +0900

    Improve the efficiency of coordinate compression

--- lib/coordComp.ml
+++ lib/coordComp.ml
@@ -6,18 +6,12 @@ module F
     val compare : t -> t -> int
   end)
 = struct
-  module IntMap = Map.Make (struct
-    type t = int
-    let compare = compare
-  end)
   module CoordMap = Map.Make (Coord)
 
   let compress cs =
-    let (n, comp, decomp) =
-      List.fold_left (fun (n, comp, decomp) c ->
-        if CoordMap.mem c comp then (n, comp, decomp)
-        else (n + 1, CoordMap.add c n comp, IntMap.add n c decomp))
-      (0, CoordMap.empty, IntMap.empty) cs in
-    (n, (fun c -> CoordMap.find c comp), (fun n -> IntMap.find n decomp))
-  let compress cs = compress @@ List.sort Coord.compare cs
+    let a =
+      Array.of_list @@
+      List.sort_uniq Coord.compare cs in
+    let n = Array.length a in
+    (n, (fun x -> Bsearch.upper_bound 0 n (fun i _ -> 0 <= Coord.compare x a.(i))), Array.get a)
 end
