4eaf4472b91b3892676e363a5ff7ddd2af0e0c93
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-07 20:42:35 +0900

    Improve the efficiency of memoizations

--- lib/memo.ml
+++ lib/memo.ml
@@ -1,19 +1,33 @@
-(* n is the initial size of hash table *)
-let memoize n f =
-  let dp = Hashtbl.create n in
-  let rec get x =
-    try Hashtbl.find dp x with
-    | Not_found ->
-        let result = f get x in
-        Hashtbl.add dp x result;
-        result in
-  get
+module F
+  (Array : sig
+    type t
+    type key
+    type elt
+    type size
+    val make : size -> t
+    val get : t -> key -> elt option
+    val set : t -> key -> elt -> unit
+  end)
+= struct
+  type dom = Array.key
+  type cod = Array.elt
+  type size = Array.size
 
-(* continuation passing style *)
-let memoize_cps n f =
-  let dp = Hashtbl.create n in
-  let rec get x k =
-    try k @@ Hashtbl.find dp x with
-    | Not_found ->
-        f get x @@ fun y ->
-          Hashtbl.add dp x y; k y in get
+  let memoize n f =
+    let dp = Array.make n in
+    let rec get i =
+      match Array.get dp i with
+      | Some x -> x
+      | None ->
+          let x = f get i in
+          Array.set dp i x; x in get
+
+  let memoize_cps n f =
+    let dp = Array.make n in
+    let rec get i k =
+      match Array.get dp i with
+      | Some x -> k x
+      | None ->
+          f get i @@ fun x ->
+            Array.set dp i x; k x in get
+end
--- test/memo.ml
+++ test/memo.ml
@@ -1,9 +1,33 @@
-let fib = Compelib.Memo.memoize 10 @@ fun fib n ->
+module M = Compelib.Memo.F (struct
+  type t = int array
+  type key = int
+  type elt = int
+  type size = int
+  let make = Fun.flip Array.make (-1)
+  let get a i =
+    let x = a.(i) in
+    if x < 0 then None else Some x
+  let set = Array.set
+end)
+
+let fib = M.memoize 10 @@ fun fib n ->
   if n <= 1 then n else fib (n - 1) + fib (n - 2)
 
 let%test _ = List.init 10 fib = [0; 1; 1; 2; 3; 5; 8; 13; 21; 34]
 
-let routes = Compelib.Memo.memoize 25 @@ fun routes (n, m) ->
+module N = Compelib.Memo.F (struct
+  type t = int array array
+  type key = int * int
+  type elt = int
+  type size = int * int
+  let make (n, m) = Array.make_matrix n m (-1)
+  let get a (i, j) =
+    let x = a.(i).(j) in
+    if x < 0 then None else Some x
+  let set a (i, j) x = a.(i).(j) <- x
+end)
+
+let routes = N.memoize (5, 5) @@ fun routes (n, m) ->
   if n = 0 || m = 0
   then 1
   else routes (n - 1, m) + routes (n, m - 1)
@@ -16,10 +40,9 @@ let%test _ =
    [1; 4; 10; 20; 35];
    [1; 5; 15; 35; 70]]
 
-(* sample code *)
 let ( +^ ) x y = (x + y) mod 1000000007
 
-let fib = Compelib.Memo.memoize_cps 1000000 @@ fun fib n k ->
+let fib = M.memoize_cps 1000000 @@ fun fib n k ->
   if n <= 1 then k 1
   else fib (n - 1) @@ fun x -> fib (n - 2) @@ fun y -> k @@ x +^ y
 
