57b216497fcb1b09032dce8617c6cc7040d290b0
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-07 17:05:53 +0900

    Add a .mli file for Warshall-Floyd algorithm

--- lib/graph/warshallFloyd.ml
+++ lib/graph/warshallFloyd.ml
@@ -1,47 +1,13 @@
 module F
   (Weight : sig
     type t
-    val inf : t (* オーバーフローの恐れはないので，max_intとか突っ込んでも良い *)
+    val inf : t
     val zero : t
-    val neg_inf : t (* オーバーフローの恐れはないので，min_intとか突っ込んでも良い *)
+    val neg_inf : t
     val ( + ) : t -> t -> t
     val compare : t -> t -> int
-  end) :
-sig
-  type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
-
-  (* あまり行儀が良くない関数達 *)
-
-  (* 隣接行列を作る関数 *)
-  val make_adjmatrix :
-    (* 頂点の数n *)
-    int ->
-    (* 辺のリスト
-       頂点は0からn-1までの整数でなくてはならない *)
-    (int * int * Weight.t) church_list ->
-    (* 隣接行列 *)
-    Weight.t array array
-
-  val raw_warshall_floyd :
-    (* 頂点の数n *)
-    int ->
-    (* 隣接行列 *)
-    Weight.t array array ->
-    (* 隣接行列に上書きして，各頂点間の最短距離を格納する *)
-    unit
-
-  (* 行儀の良い関数 *)
-
-  val warshall_floyd :
-    (* 頂点の数n *)
-    int ->
-    (* 辺のリスト
-       頂点は0からn-1までの整数でなくてはならない *)
-    (int * int * Weight.t) church_list ->
-    (* 辿り着けなければinf，負閉路を含む経路があればneg_infを返す関数 *)
-    (int -> int -> Weight.t)
-end =
-struct
+  end)
+= struct
   type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
 
   let make_adjmatrix n es =
