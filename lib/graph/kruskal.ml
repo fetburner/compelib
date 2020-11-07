387d612bcb94ff13a0ca683d5589cfdf1ea2b2a5
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-08 04:19:49 +0900

    Refactor the interface of Kruskal's algorithm

--- lib/graph/kruskal.ml
+++ lib/graph/kruskal.ml
@@ -9,14 +9,11 @@ module F
     let union _ _ = ()
   end)
 
-  type 'a church_list = { fold : 'b. ('a -> 'b -> 'b) -> 'b -> 'b }
-
-  let kruskal n es =
-    { fold = fun f init ->
-      let uf = Array.init n @@ fun _ -> UF.make () in
-      List.fold_left (fun acc (u, v, w) ->
-        if UF.equal uf.(u) uf.(v)
-        then acc
-        else (UF.unite uf.(u) uf.(v); f (u, v, w) acc)) init @@
-      List.sort (fun (_, _, w) (_, _, w') -> Weight.compare w w') es }
+  let kruskal n es f acc =
+    let uf = Array.init n @@ fun _ -> UF.make () in
+    List.fold_left (fun acc (u, v, w) ->
+      if UF.equal uf.(u) uf.(v)
+      then acc
+      else (UF.unite uf.(u) uf.(v); f u v w acc)) acc @@
+    List.sort (fun (_, _, w) (_, _, w') -> Weight.compare w w') es 
 end
