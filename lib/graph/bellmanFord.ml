3f78cbeab74e623fb0b04e9d3512e9950d2d7ecf
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-10 13:56:18 +0900

    Modify the relax scheduling of Bellman-Ford algorithm

--- lib/graph/bellmanFord.ml
+++ lib/graph/bellmanFord.ml
@@ -9,47 +9,49 @@ module F
   end)
 = struct
   let bellman_ford n es s =
+    let es = List.sort (fun (u, v, _) (u', v', _) ->
+      match compare u v, compare u' v' with
+      (* 自己辺同士は始点の小さい順に緩和 *)
+      | 0, 0 -> compare u u'
+      (* 添字の小さい頂点から大きい頂点への辺同士は，始点の小さい順に緩和 *)
+      | x, y when x < 0 && y < 0 -> compare u u'
+      (* 添字の大きい頂点から小さい頂点への辺同士は，始点の大きい順に緩和 *)
+      | x, y when x > 0 && y > 0 -> compare u u'
+      (* 自己辺は添字の小さい頂点から大きい頂点への辺と一緒のタイミングで緩和する *)
+      | 0, y when y < 0 ->
+          begin match compare u u' with
+          | 0 -> -1
+          | x -> x
+          end
+      | x, 0 when x < 0 ->
+          begin match compare u u' with
+          | 0 -> 1
+          | x -> x
+          end
+      (* 添字の小さい頂点から大きい頂点への辺の緩和が終わってから，
+         添字の大きい頂点から小さい頂点への辺を緩和する *)
+      | x, y when x < 0 && y > 0 -> -1
+      | x, y when x > 0 && y < 0 -> 1
+      | 0, y when y > 0 -> -1
+      | x, 0 when x > 0 -> 1
+      | _, _ -> failwith "bellman_ford") es in
     (* 距離を覚えるやつ *)
     let d = Array.make n Weight.inf in
-    (* 自己辺の最小のコスト *)
-    let self = Array.make n Weight.zero in
-    (* 始点より終点のインデックスが大きくなる辺を集めた隣接リスト *)
-    let inc = Array.make n [] in
-    (* 始点より終点のインデックスが小さくなる辺を集めた隣接リスト *)
-    let dec = Array.make n [] in
-    es (fun u v c ->
-      match compare v u with
-      | 0 -> if 0 < Weight.compare self.(u) c then self.(u) <- c
-      | cmp when 0 < cmp -> inc.(u) <- (v, c) :: inc.(u)
-      | _ -> dec.(u) <- (v, c) :: dec.(u));
-    (* 自己辺は最初に緩和する必要がある *)
-    Array.iteri (fun v c ->
-      if 0 < Weight.compare Weight.zero c then
-        inc.(v) <- (v, c) :: inc.(v)) self;
     d.(s) <- Weight.zero;
     (* 残りの反復回数 *)
     let i = ref n in
     (* 更新が行われたか *)
     let is_modified = ref true in
-    (* 各辺の処理 *)
-    let f u (v, c) =
-      let open Weight in
-      (* 原点から到達できない頂点は更新しない *)
-      if 0 < Weight.compare inf d.(u) then
-        let dv = if 0 <= Weight.compare neg_inf d.(u) then neg_inf else d.(u) + c in
-        if 0 < Weight.compare d.(v) dv then
-          (is_modified := true; d.(v) <- if !i <= succ n lsr 1 then neg_inf else dv) in
     while 0 < !i && !is_modified do
       is_modified := false;
-      (* インデックスが増加する辺の処理 *)
-      for u = 0 to n - 1 do
-        List.iter (f u) inc.(u)
-      done;
-      (* インデックスが減少する辺の処理 *)
-      for u = n - 1 downto 0 do
-        List.iter (f u) dec.(u)
-      done;
-      decr i
+      List.iter (fun (u, v, c) ->
+        let open Weight in
+        (* 原点から到達できない頂点は更新しない *)
+        if 0 < Weight.compare inf d.(u) then
+          let dv = if 0 <= Weight.compare neg_inf d.(u) then neg_inf else d.(u) + c in
+          if 0 < Weight.compare d.(v) dv then
+            (is_modified := true; d.(v) <- if !i <= succ n lsr 1 then neg_inf else dv)) es;
+        decr i
     done;
     Array.get d
 end
