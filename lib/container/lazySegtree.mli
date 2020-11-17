3ebb97ec4deada0ff003c4240381560253736f7a
Masayuki Mizuno <mizuno@wantedly.com>
2020-11-17 16:13:42 +0900

    Rewrite the lazy segment tree

--- lib/container/lazySegtree.mli
+++ lib/container/lazySegtree.mli
@@ -1,27 +1,43 @@
-module type Monoid = sig
-  type t
-  val e : t
-  val op : t -> t -> t
-end
-
-module Make (S : Monoid) : sig
+module Make
+  (* 半群 *)
+  (S : sig
+    type t
+    val op : t -> t -> t
+  end)
+  (* 半群の自己同型写像 *)
+  (F : sig
+    type t
+    type dom = S.t
+    type cod = S.t
+    (* 恒等写像 *)
+    val id : t
+    (* 写像の合成 *)
+    val comp : t -> t -> t
+    (* 半群の要素に写像を適用する *)
+    val apply : t -> dom -> cod
+  end)
+: sig
   type t
   type elt = S.t
-  (* n要素の単位元からなるセグ木を作る *)
-  val make : int -> t
+  type map = F.t
+
+  (* 与えられたリストの要素からなる遅延セグ木を作る *)
+  val of_list : elt list -> t
+  (* f 0, ... f (n - 1)のn要素からなる遅延セグ木を作る *)
+  val init : int -> (int -> elt) -> t
   (* 
    * update i f t
-   * i番目の要素にfを適用したセグ木を作る
+   * i 番目の要素に f を適用したセグ木を作る
    *)
   val update : int -> (elt -> elt) -> t -> t
   (*
    * update_range l r f t
-   * [l, r)の要素に自己準同型写像fを適用したセグ木を作る
+   * [l, r) の要素に自己同型写像 f を適用したセグ木を作る
    *)
-  val update_range : int -> int -> (elt -> elt) -> t -> t
+  val update_range : int -> int -> map -> t -> t
   (*
    * query l r t
-   * [l, r)の要素の積を求める
+   * [l, r) の要素の積を求める
    *)
   val query : int -> int -> t -> elt
 end
