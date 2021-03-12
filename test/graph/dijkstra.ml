9584f0f031d744116b3374c4ed2e463f0187abc5
Masayuki Mizuno <mizuno@wantedly.com>
2021-03-13 06:15:56 +0900

    Modify the interface of Dijkstra's algorithm

--- test/graph/dijkstra.ml
+++ test/graph/dijkstra.ml
@@ -9,20 +9,10 @@ module IntMap = Map.Make (Int)
 
 module G = Compelib.Dijkstra.F
   (struct
-    type t = int array
-    type key = int
-    type elt = int
-    type size = int
-    let make = Fun.flip Array.make max_int
-    let get = Array.get
-    let set = Array.set
-  end)
-  (struct
     type t = int list IntMap.t ref
     type elt = int
     type key = int
-    type size = int
-    let make _ = ref IntMap.empty
+    let create () = ref IntMap.empty
     let take_min_binding q =
       match IntMap.min_binding !q with
       | exception Not_found -> None
@@ -41,67 +31,15 @@ let e =
     [ (0, 14); (2, 2); (4, 9) ]|]
 
 let%test _ =
+  let d = Array.make 7 max_int in
   List.init 7 (G.shortest_path
     (module struct
       module Distance = Int
       module Vertex = struct
         type t = int
-        type set = int
-        let universe = 7
-        let iter_adjacencies u f = Fun.flip List.iter e.(u) @@ fun (v, c) -> f v @@ ( + ) c
+        let get_distance = Array.get d
+        let set_distance = Array.set d
+        let iter_adjacencies u f = Fun.flip List.iter e.(u) @@ fun (v, c) -> f v @@ d.(u) + c
       end
     end) 0)
   = [ 0; 7; 9; 20; 20; 11; max_int ]
-
-(* 経路長をMapに保存する *)
-module G' = Compelib.Dijkstra.F
-  (struct
-    type t = int IntMap.t ref
-    type key = int
-    type elt = int
-    type size = unit
-    let make () = ref IntMap.empty
-    let get m k = try IntMap.find k !m with Not_found -> max_int
-    let set m k x = m := IntMap.add k x !m
-  end)
-  (struct
-    type t = int list IntMap.t ref
-    type elt = int
-    type key = int
-    type size = unit
-    let make () = ref IntMap.empty
-    let take_min_binding q =
-      match IntMap.min_binding !q with
-      | exception Not_found -> None
-      | (w, [v]) -> q := IntMap.remove w !q; Some (w, v)
-      | (w, v :: vs) -> q := IntMap.add w vs !q; Some (w, v)
-    let add q w v  =
-      q := IntMap.update w (fun vs -> Some (v :: Option.value ~default:[] vs)) !q
-  end)
-
-let%test _ =
-  List.init 10 (G'.shortest_path
-    (module struct
-      module Distance = Int
-      module Vertex = struct
-        type t = int
-        type set = unit
-        let universe = ()
-        let iter_adjacencies u f = Fun.flip List.iter e.(u) @@ fun (v, c) -> f v @@ ( + ) c
-      end
-    end) 0)
-  = [ 0; 7; 9; 20; 20; 11; max_int; max_int; max_int; max_int ]
-
-(* 無限グラフも可 *)
-let%test _ =
-  List.init 10 (G'.shortest_path
-    (module struct
-      module Distance = Int
-      module Vertex = struct
-        type t = int
-        type set = unit
-        let universe = ()
-        let iter_adjacencies u f = f (u + 1) succ
-      end
-    end) 0)
-  = [0; 1; 2; 3; 4; 5; 6; 7; 8; 9]
