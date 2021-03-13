5168e7d240cc4c52c5d06861704aa0dde6d2582a
Masayuki Mizuno <mizuno@wantedly.com>
2021-03-13 20:32:18 +0900

    Modify the interface of Bellman-Ford algorithm

--- lib/graph/bellmanFord.ml
+++ lib/graph/bellmanFord.ml
@@ -1,16 +1,16 @@
 module type WeightedDirectedGraph = sig
-  module Vertex : sig
-    type t
-    type set
-    val universe : set
-    val cardinal_universe : int
-    val compare : t -> t -> int
-  end
   module Distance : sig
     type t
     val inf : t
     val zero : t
     val neg_inf : t
+    val compare : t -> t -> int
+  end
+  module Vertex : sig
+    type t
+    val cardinal : int
+    val get_distance : t -> Distance.t
+    val set_distance : t -> Distance.t -> unit
     val compare : t -> t -> int
   end
   module Edge : sig
@@ -22,79 +22,60 @@ module type WeightedDirectedGraph = sig
   end
 end
 
-module F
-  (Array : sig
-    type t
-    type key
-    type elt
-    type size
-    val make : size -> t
-    val get : t -> key -> elt
-    val set : t -> key -> elt -> unit
-  end)
-= struct
-  type vertex = Array.key
-  type distance = Array.elt
-  type vertices = Array.size
-
-  let shortest_path
-    (module G : WeightedDirectedGraph
-      with type Vertex.t = vertex
-       and type Vertex.set = vertices
-       and type Distance.t = distance) s =
-    let es = List.sort (fun e e' ->
-      let u = G.Edge.source e in
-      let v = G.Edge.destination e in
-      let u' = G.Edge.source e' in
-      let v' = G.Edge.destination e' in
-      match G.Vertex.compare u v, G.Vertex.compare u' v' with
-      (* 自己辺同士は始点の小さい順に緩和 *)
-      | 0, 0 -> G.Vertex.compare u u'
-      (* 添字の小さい頂点から大きい頂点への辺同士は，始点の小さい順に緩和 *)
-      | x, y when x < 0 && y < 0 -> G.Vertex.compare u u'
-      (* 添字の大きい頂点から小さい頂点への辺同士は，始点の大きい順に緩和 *)
-      | x, y when x > 0 && y > 0 -> G.Vertex.compare u' u
-      (* 自己辺は添字の小さい頂点から大きい頂点への辺と一緒のタイミングで緩和する *)
-      | 0, y when y < 0 ->
-          begin match G.Vertex.compare u u' with
-          | 0 -> -1
-          | x -> x
-          end
-      | x, 0 when x < 0 ->
-          begin match G.Vertex.compare u u' with
-          | 0 -> 1
-          | x -> x
-          end
-      (* 添字の小さい頂点から大きい頂点への辺の緩和が終わってから，
-         添字の大きい頂点から小さい頂点への辺を緩和する *)
-      | x, y when x < 0 && y > 0 -> -1
-      | x, y when x > 0 && y < 0 -> 1
-      | 0, y when y > 0 -> -1
-      | x, 0 when x > 0 -> 1
-      | _, _ -> failwith "bellman_ford") G.Edge.universe in
-    (* 距離を覚えるやつ *)
-    let d = Array.make G.Vertex.universe in
-    Array.set d s G.Distance.zero;
-    (* 残りの反復回数 *)
-    let i = ref G.Vertex.cardinal_universe in
-    (* 負閉路とみなす閾値 *)
-    let th = succ G.Vertex.cardinal_universe lsr 1 in
-    (* 更新が行われたか *)
-    let is_modified = ref true in
-    while 0 < !i && !is_modified do
-      is_modified := false;
-      List.iter (fun e ->
-        (* 原点から到達できない頂点は更新しない *)
-        let du = Array.get d (G.Edge.source e) in
-        if 0 < G.Distance.compare G.Distance.inf du then
-          let v = G.Edge.destination e in
-          let dv =
-            if 0 <= G.Distance.compare G.Distance.neg_inf du
-            then G.Distance.neg_inf
-            else G.Edge.add_weight e du in
-          if 0 < G.Distance.compare (Array.get d v) dv then
-            (is_modified := true; Array.set d v @@ if !i <= th then G.Distance.neg_inf else dv)) es;
-        decr i
-    done;
-    Array.get d
-end
+let shortest_path (type vertex) (type distance)
+  (module G : WeightedDirectedGraph
+    with type Vertex.t = vertex
+     and type Distance.t = distance) s =
+  let es = List.sort (fun e e' ->
+    let u = G.Edge.source e in
+    let v = G.Edge.destination e in
+    let u' = G.Edge.source e' in
+    let v' = G.Edge.destination e' in
+    match G.Vertex.compare u v, G.Vertex.compare u' v' with
+    (* 自己辺同士は始点の小さい順に緩和 *)
+    | 0, 0 -> G.Vertex.compare u u'
+    (* 添字の小さい頂点から大きい頂点への辺同士は，始点の小さい順に緩和 *)
+    | x, y when x < 0 && y < 0 -> G.Vertex.compare u u'
+    (* 添字の大きい頂点から小さい頂点への辺同士は，始点の大きい順に緩和 *)
+    | x, y when x > 0 && y > 0 -> G.Vertex.compare u' u
+    (* 自己辺は添字の小さい頂点から大きい頂点への辺と一緒のタイミングで緩和する *)
+    | 0, y when y < 0 ->
+        begin match G.Vertex.compare u u' with
+        | 0 -> -1
+        | x -> x
+        end
+    | x, 0 when x < 0 ->
+        begin match G.Vertex.compare u u' with
+        | 0 -> 1
+        | x -> x
+        end
+    (* 添字の小さい頂点から大きい頂点への辺の緩和が終わってから，
+       添字の大きい頂点から小さい頂点への辺を緩和する *)
+    | x, y when x < 0 && y > 0 -> -1
+    | x, y when x > 0 && y < 0 -> 1
+    | 0, y when y > 0 -> -1
+    | x, 0 when x > 0 -> 1
+    | _, _ -> failwith "bellman_ford") G.Edge.universe in
+  G.Vertex.set_distance s G.Distance.zero;
+  (* 残りの反復回数 *)
+  let i = ref G.Vertex.cardinal in
+  (* 負閉路とみなす閾値 *)
+  let th = succ G.Vertex.cardinal lsr 1 in
+  (* 更新が行われたか *)
+  let is_modified = ref true in
+  while 0 < !i && !is_modified do
+    is_modified := false;
+    List.iter (fun e ->
+      (* 原点から到達できない頂点は更新しない *)
+      let du = G.Vertex.get_distance (G.Edge.source e) in
+      if 0 < G.Distance.compare G.Distance.inf du then
+        let v = G.Edge.destination e in
+        let dv =
+          if 0 <= G.Distance.compare G.Distance.neg_inf du
+          then G.Distance.neg_inf
+          else G.Edge.add_weight e du in
+        if 0 < G.Distance.compare (G.Vertex.get_distance v) dv then
+          (is_modified := true; G.Vertex.set_distance v @@ if !i <= th then G.Distance.neg_inf else dv)) es;
+      decr i
+  done;
+  G.Vertex.get_distance
--- lib/graph/bellmanFord.mli
+++ lib/graph/bellmanFord.mli
@@ -1,16 +1,18 @@
 module type WeightedDirectedGraph = sig
-  module Vertex : sig
-    type t
-    type set
-    val universe : set (* 頂点の全体集合 *)
-    val cardinal_universe : int (* 頂点数 *)
-    val compare : t -> t -> int
-  end
   module Distance : sig
     type t
     val inf : t (* オーバーフローの恐れはないので，max_intとか突っ込んでも良い *)
     val zero : t
     val neg_inf : t  (* オーバーフローの恐れはないので，min_intとか突っ込んでも良い *)
+    val compare : t -> t -> int
+  end
+  module Vertex : sig
+    type t
+    val cardinal : int (* 頂点数 *)
+    (* 現在の頂点への距離を取得 *)
+    val get_distance : t -> Distance.t
+    (* 現在の頂点への距離を代入 *)
+    val set_distance : t -> Distance.t -> unit
     val compare : t -> t -> int
   end
   module Edge : sig
@@ -23,32 +25,13 @@ module type WeightedDirectedGraph = sig
   end
 end
 
-module F
-  (* 頂点を添字，経路長を要素とした配列の実装 *)
-  (Array : sig
-    type t
-    type key
-    type elt
-    type size
-    (* 無限大 で初期化された配列を作る *)
-    val make : size -> t
-    val get : t -> key -> elt
-    val set : t -> key -> elt -> unit
-  end) :
-sig
-  type vertex = Array.key
-  type distance = Array.elt
-  type vertices = Array.size
-
   val shortest_path :
     (module WeightedDirectedGraph
-      with type Vertex.t = vertex
-       and type Vertex.set = vertices
-       and type Distance.t = distance) ->
+      with type Vertex.t = 'vertex
+       and type Distance.t = 'distance) ->
     (* 始点 *)
-    vertex ->
+    'vertex ->
     (* 頂点を受け取り，そこまでの距離を返す関数
        頂点に到達するパスが無ければ inf を返し，
        そこに到達するまでに負閉路があれば neg_inf を返す *)
-    (vertex -> distance)
-end
+    ('vertex -> 'distance)
