de9e6b602191510aabee1c03bb1b2db66f4d86eb
Masayuki Mizuno <mizuno@wantedly.com>
2021-03-14 22:58:42 +0900

    Modify the interface of 01-BFS

--- lib/graph/bfs01.ml
+++ lib/graph/bfs01.ml
@@ -1,58 +1,39 @@
 module type Weighted01DirectedGraph = sig
   module Vertex : sig
     type t
-    type set
-    val universe : set
+    val get_distance : t -> int
+    val set_distance : t -> int -> unit
     val iter_adjacencies : t -> f0:(t -> unit) -> f1:(t -> unit) -> unit
   end
 end
 
-module F
-  (Array : sig
-    type t
-    type key
-    type size
-    type elt = int
-    val make : size -> t
-    val get : t -> key -> elt
-    val set : t -> key -> elt -> unit
-  end)
-= struct
-  type distance = int
-  type vertex = Array.key
-  type vertices = Array.size
-
-  let shortest_path
-    (module G : Weighted01DirectedGraph
-      with type Vertex.t = vertex
-       and type Vertex.set = vertices) s =
-    let d = Array.make G.Vertex.universe in
-    (* 始点への経路長を0にする *)
-    Array.set d s 0;
-    let w = ref 1 in
-    let q = ref [s] in
-    let rec bfs t =
-      match !q with
-      (* もう既に全ての頂点までの経路が分かっているので返す *)
-      | [] -> Array.get d t
-      | us ->
-          match Array.get d t with
-          (* 既に終点までの距離が分かっているので返す *)
-          | x when x < !w -> x
-          (* 終点までの距離が分かっていないので，BFSを続行 *)
-          | _ ->
-              q := [];
-              List.iter dfs us;
-              incr w;
-              bfs t
-    and dfs u =
-      G.Vertex.iter_adjacencies u
-        ~f0:(fun v ->
-          if !w <= Array.get d v then
-            (Array.set d v (!w - 1); dfs v))
-        ~f1:(fun v ->
-          if !w < Array.get d v then
-            (Array.set d v !w; q := v :: !q)) in
-    bfs
-end
-
+let shortest_path (type vertex)
+  (module G : Weighted01DirectedGraph
+    with type Vertex.t = vertex) s =
+  (* 始点への経路長を0にする *)
+  G.Vertex.set_distance s 0;
+  let w = ref 1 in
+  let q = ref [s] in
+  let rec bfs t =
+    match !q with
+    (* もう既に全ての頂点までの経路が分かっているので返す *)
+    | [] -> G.Vertex.get_distance t
+    | us ->
+        match G.Vertex.get_distance t with
+        (* 既に終点までの距離が分かっているので返す *)
+        | x when x < !w -> x
+        (* 終点までの距離が分かっていないので，BFSを続行 *)
+        | _ ->
+            q := [];
+            List.iter dfs us;
+            incr w;
+            bfs t
+  and dfs u =
+    G.Vertex.iter_adjacencies u
+      ~f0:(fun v ->
+        if !w <= G.Vertex.get_distance v then
+          (G.Vertex.set_distance v (!w - 1); dfs v))
+      ~f1:(fun v ->
+        if !w < G.Vertex.get_distance v then
+          (G.Vertex.set_distance v !w; q := v :: !q)) in
+  bfs
