4fa68e8e3df7243c4034d17c1b0e9fc85ec012cd
Masayuki Mizuno <mizuno@wantedly.com>
2021-07-14 01:53:50 +0900

    Refactor the correctness of Floyd's cycle detection algorithm

--- lib/CycleDetection.v
+++ lib/CycleDetection.v
@@ -40,8 +40,8 @@ Section CycleDetection.
       remember (n - m) as d.
       generalize dependent m.
       induction d as [ | ? ] => m ?; constructor => ? [ ? Hnp ]; subst.
-      + (have : (n < S m) by lia) => /Hnp. congruence.
-      + apply /IHd. lia.
+      + by have /Hnp /(_ Hmeet) : n < S m by lia.
+      + apply /IHd. by lia.
     - by case (le_lt_dec n n') => // Hlt /(Hneq _ Hlt).
     - move => m /le_S_n Hle.
       by case (le_lt_dec n m) => [ /(@Nat.le_antisymm _ _ Hle) -> | /Hneq ].
@@ -58,7 +58,7 @@ Section CycleDetection.
     { move: Hperiod => [ mu' [ l' [ [ Hpos Hperiod ] Hleast ] ] ].
       exists ((mu' + 2) * l' - 1).
       set j := (mu' + 2) * l' - 1.
-      have -> : (fun x => f (f x)) = @iter 2 _ f by [].
+      have -> : (fun x => f (f x)) = @iter 2 _ f := eq_refl.
       rewrite -iter_mult Nat.mul_comm /= Nat.add_0_r !iter_S.
       have -> : S (S (j + j)) = (mu' + 2) * l' + S j by lia.
       elim: (mu' + 2) => [ // | ? ? /= ].
@@ -66,12 +66,12 @@ Section CycleDetection.
     case (tortoise_and_hare f u v) => [ | [ mu w ] Hmu ].
     { exists (S m).
       move: Hm => [ -> [ ] ].
-      have -> : (fun x => f (f x)) = Nat.iter 2 f by [].
+      have -> : (fun x => f (f x)) = Nat.iter 2 f := eq_refl.
       by rewrite -iter_plus !iter_S -!iter_mult -(Nat.mul_comm 2) /= Nat.add_0_r Nat.add_succ_r. }
     case (tortoise_and_hare id (f w) w) => [ | [ l ? ] Hl ].
     { exists m.
       move: Hm Hmu => [ -> [ ? ? ] ] [ -> [ Heq ? ] ].
-      have -> : id = Nat.iter 0 f by [].
+      have -> : id = Nat.iter 0 f := eq_refl.
       by rewrite -iter_mult -(Nat.mul_comm 0) Heq !iter_S -iter_plus Nat.add_comm iter_plus -iter_S Heq. }
     exists (mu, S l).
     move: Hperiod => [ mu' [ l' [ [ Hpos Hperiod ] Hleast ] ] ].
@@ -81,7 +81,7 @@ Section CycleDetection.
       have -> : S l + (m0 - mu + mu) = (m0 - mu) + (S l + mu) by lia.
       move: Hmu Hl => [ -> [ -> ? ] ] [ -> [ ] ].
       rewrite !iter_plus -iter_S => ->.
-      have -> : id = Nat.iter 0 f by [].
+      have -> : id = Nat.iter 0 f := eq_refl.
       by rewrite -iter_mult Nat.mul_0_r. }
     have /Hleast [ Hlemu Hlem ] : eventually_periodic mu (S m) v.
     { split => [ | m0 ? ]; auto with arith.
@@ -100,7 +100,7 @@ Section CycleDetection.
       have : S l <= l' => [ | /(le_antisym _ _ Hlel) <- // ].
       { rewrite (S_pred_pos l') //.
         apply /le_n_S /(proj2 (proj2 Hl)).
-        have -> : id = Nat.iter 0 f by [].
+        have -> : id = Nat.iter 0 f := eq_refl.
         move: Hm Hmu => [ -> [ ? ? ] ] [ -> [ ? ? ] ].
         rewrite 2!iter_S -S_pred_pos // -iter_mult Nat.mul_0_r -!iter_plus -plus_assoc Hperiod //.
         lia. }
@@ -109,13 +109,13 @@ Section CycleDetection.
         - move: Hm Hmu => [ -> [ ? ? ] ] [ -> [ ] ].
           by rewrite Nat.mul_0_l Nat.sub_0_r Nat.add_comm iter_plus iter_S /= iter_S.
         - rewrite -IH. { lia. }
-          have->: S m - i * l' + mu = l' + (S m - S i * l' + mu) by lia.
+          have -> : S m - i * l' + mu = l' + (S m - S i * l' + mu) by lia.
           rewrite Hperiod //. lia. }
       rewrite (mult_comm _ l') -Nat.mod_eq => [ | Hperiod' ]. { lia. }
       have : eventually_periodic mu (S m mod l') v => [ | /Hleast [ ? /le_not_lt [ ] ] ].
       { split => [ // | m0 ? ].
-        have->: m0 = (m0 - mu) + mu by lia.
-        have->: S m mod l' + (m0 - mu + mu) = (m0 - mu) + (S m mod l' + mu) by lia.
+        have -> : m0 = (m0 - mu) + mu by lia.
+        have -> : S m mod l' + (m0 - mu + mu) = (m0 - mu) + (S m mod l' + mu) by lia.
         rewrite !(@iter_plus _ _ _ _ (m0 - mu)) Hperiod' //.
         apply /Nat.mul_div_le. lia. }
       apply /Nat.mod_upper_bound. lia.
