03c266e32c0be0e94628ea3681c01bd5a4401599
Masayuki Mizuno <mizuno@wantedly.com>
2022-06-12 03:07:56 +0900

    Rewrite segment tree

--- lib/container/segtree.ml
+++ lib/container/segtree.ml
@@ -1,60 +1,52 @@
-module F
-  (M : sig
-    type t
-    val e : t
-    val op : t -> t -> t
-  end)
-= struct
-  type elt = M.t
-  type t = { n : int; size : int; log : int; d : M.t array }
+module type Monoid = sig
+  type t
 
-  let update d k = d.(k) <- M.op d.(k lsl 1) d.(k lsl 1 + 1)
+  val e : t
+  val op : t -> t -> t
+end
+
+module Make (M : Monoid) = struct
+  type elt = M.t
+  type t = elt array
+
+  let make m = Array.make (2 * m) M.e
+  let leaves segtree = Array.length segtree / 2
+
+  let rec initialize_ancestors i j n d =
+    if 0 < n then begin
+      for k = 0 to n - 1 do
+        d.(i + k) <- M.op d.(j + 2 * k) d.(j + 2 * k + 1)
+      done;
+      initialize_ancestors (i + n) i (n / 2) d
+    end
 
   let of_list l =
-    let n = List.length l in
-    let log = Bits.ceil_log2 n in
-    let size = 1 lsl log in
-    let d = Array.make (size lsl 1) M.e in
-    List.iteri (fun i -> Array.set d (size + i)) l;
-    for i = size - 1 downto 1 do
-      update d i
+    let segtree = make (List.length l) in
+    List.iteri (Array.set segtree) l;
+    initialize_ancestors (leaves segtree) 0 (leaves segtree / 2) segtree;
+    segtree
+
+  let init m f =
+    let segtree = make m in
+    for i = 0 to m - 1 do
+      segtree.(i) <- f i
     done;
-    { n; size; log; d }
+    initialize_ancestors m 0 (m / 2) segtree;
+    segtree
 
-  let init n f =
-    let log = Bits.ceil_log2 n in
-    let size = 1 lsl log in
-    let d = Array.make (size lsl 1) M.e in
-    for i = 0 to n - 1 do
-      d.(size + i) <- f i
-    done;
-    for i = size - 1 downto 1 do
-      update d i
-    done;
-    { n; size; log; d }
+  let product segtree = SegtreeQueries.product_iter M.e M.op (Array.get segtree) (leaves segtree)
+  let upper_bound segtree l p = SegtreeQueries.upper_bound_iter M.e M.op p (Array.get segtree) (leaves segtree) l
+  let lower_bound segtree r p = SegtreeQueries.lower_bound_iter M.e M.op p (Array.get segtree) (leaves segtree) r
 
-  let get i { d; size; n; _ } =
-    assert (0 <= i && i < n);
-    d.(i + size)
+  let rec update_ancestors i j k n d =
+    if k < n then begin
+      d.(i + k) <- M.op d.(j + 2 * k) d.(j + 2 * k + 1);
+      update_ancestors (i + n) i (k / 2) (n / 2) d
+    end
+  let update segtree i f =
+    segtree.(i) <- f segtree.(i);
+    update_ancestors (leaves segtree) 0 (i / 2) (leaves segtree / 2) segtree
 
-  let query l r t =
-    assert (0 <= l && l <= r && r <= t.n);
-    let rec query_aux l r sml smr =
-      if r <= l
-      then M.op sml smr
-      else
-        let l, sml = if l land 1 = 0 then l, sml else l + 1, M.op sml t.d.(l) in
-        let r, smr = if r land 1 = 0 then r, smr else r - 1, M.op t.d.(r - 1) smr in
-        query_aux (l lsr 1) (r lsr 1) sml smr in
-    query_aux (l + t.size) (r + t.size) M.e M.e
-
-  let update i f t =
-    assert (0 <= i && i < t.n);
-    let i = i + t.size in
-    t.d.(i) <- f t.d.(i);
-    for j = 1 to t.log do
-      update t.d (i lsr j)
-    done
-
-  let set i x = update i @@ Fun.const x
+  let get = Array.get
+  let set segtree i x = update segtree i @@ Fun.const x
 end
--- lib/container/segtree.mli
+++ lib/container/segtree.mli
@@ -1,10 +1,11 @@
-module F
-  (M : sig
-    type t
-    val e : t
-    val op : t -> t -> t
-  end)
-: sig
+module type Monoid = sig
+  type t
+
+  val e : t
+  val op : t -> t -> t
+end
+
+module Make (M : Monoid) : sig
   type t
   type elt = M.t
 
@@ -12,12 +13,16 @@ module F
   val of_list : elt list -> t
   (* f 0, ... f (n - 1)のn要素からなるセグ木を作る *)
   val init : int -> (int -> elt) -> t
-  (* [get i t]: i番目の要素を返す *)
-  val get : int -> t -> elt
-  (* [query l r t]: 添字が[l, r)の要素を半群の演算子で畳み込んだ値を求める *)
-  val query : int -> int -> t -> elt
-  (* [set i x t]: i番目の要素をxに破壊的に変更する *)
-  val set : int -> elt -> t -> unit
-  (* [update i f t]: i番目の要素x_iをf x_iに破壊的に変更する *)
-  val update : int -> (elt -> elt) -> t -> unit
+  (* [get t i]: i番目の要素を返す *)
+  val get : t -> int -> elt
+  (* [product t l r]: 添字が[l, r)の要素を半群の演算子で畳み込んだ値を求める *)
+  val product : t -> int -> int -> elt
+  (* [upper_bound t l p]: [l, r) の区間積が述語 p を満たすような最大の右端 r と区間積を返す *)
+  val upper_bound : t -> int -> (elt -> bool) -> int * elt
+  (* [lower_bound t r p]: [l, r) の区間積が述語 p を満たすような最大の左端 l と区間積を返す *)
+  val lower_bound : t -> int -> (elt -> bool) -> int * elt
+  (* [set t i x]: i番目の要素をxに破壊的に変更する *)
+  val set : t -> int -> elt -> unit
+  (* [update t i f]: i番目の要素x_iをf x_iに破壊的に変更する *)
+  val update : t -> int -> (elt -> elt) -> unit
 end
